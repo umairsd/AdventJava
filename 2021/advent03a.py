60197749b42b4c06438b0616b7b7087023eac027
Umair Saeed <umair.saeed@gmail.com>
2021-12-03 05:57:16 -0800

    [AdventOfCode] Solution to Day-03, P1.

---
+++ 2021/advent03a.py
@@ -0,0 +1,80 @@
+# Advent of Code 2021
+# Day 3. Problem a
+
+def parseLine(line):
+  lineArray = []
+  for value in line:
+    if value == '1':
+      lineArray.append(1)
+    elif value == '0':
+      lineArray.append(-1)
+  return lineArray
+
+# Assumes: Lines are the same length.
+def parseFileAndCompute(fileName):
+  counts = []
+  isFirstLine = True
+  with open(fileName) as file:
+    for line in file:
+      if isFirstLine:
+        counts = parseLine(line)
+        isFirstLine = False
+      else:
+        lineArray = parseLine(line)
+        for i in range(0, len(lineArray)):
+          counts[i] += lineArray[i]
+
+  return counts
+
+
+def calculate(counts):
+  gamma = ""
+  epsilon = ""
+  for num in counts:
+    if num > 0:
+      gamma += '1'
+      epsilon += '0'
+
+    elif num < 0:
+      gamma += '0'
+      epsilon += '1'
+
+
+  result = int(gamma, base=2) * int(epsilon, base=2)
+  return result
+
+
+
+
+
+
+
+  # aim = 0
+  # horizontal = 0
+  # depth = 0
+
+  # with open(fileName) as file:
+  #   for line in file:
+  #     tokens = line.split(" ")
+  #     direction = tokens[0]
+  #     value = int(tokens[1])
+
+  #     if direction == 'forward':
+  #       horizontal += value
+  #       depth += aim * value
+  #     elif direction == 'up':
+  #       aim -= value
+  #     elif direction == 'down':
+  #       aim += value
+
+  # return horizontal * depth
+
+
+def advent03a():
+  print("**Advent of Code - 03a")
+  data = parseFileAndCompute('advent03.txt')
+  result = calculate(data)
+  print(result)
+
+
+advent03a()
