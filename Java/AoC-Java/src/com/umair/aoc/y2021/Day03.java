6cce3d8c3c3df74bb67a7bde387ee75f85f112b7
Umair Saeed <umair.saeed@gmail.com>
2022-11-24 07:08:24 -0800

    [AoC][Java][2021] Day03, part 1.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day03.java
@@ -0,0 +1,76 @@
+package com.umair.aoc.y2021;
+
+import com.umair.aoc.common.Constants;
+import com.umair.aoc.common.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day03 extends Day {
+
+  public Day03() {
+    super(3, 2021);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    if (lines.isEmpty()) {
+      return Constants.INPUT_EMPTY;
+    }
+
+    int[] result = countsOneBitPerPosition(lines);
+    long gamma = 0;
+    long epsilon = 0;
+
+    for (int i = 0; i < result.length; i++) {
+      gamma = gamma << 1;
+      epsilon = epsilon << 1;
+      if (result[i] > 0) {
+        // Most common bit is a 1.
+        gamma = gamma | 1;
+      } else {
+        // Most common bit is a 0.
+        epsilon = epsilon | 1;
+      }
+    }
+
+    long powerConsumption = gamma * epsilon;
+    return Long.toString(powerConsumption);
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return filenameFromDataFileNumber(2);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return filenameFromDataFileNumber(1);
+  }
+
+  /**
+   * Counts the *net* count of the 1 bit at each position. Net count of 1s in a position is
+   * obtained by the (total count of 1s - total count of 0s).
+   * <p></p>
+   * Assumes: each element in `lines` is the same width.
+   */
+  private static int[] countsOneBitPerPosition(List<String> lines) {
+    if (lines.isEmpty()) { throw new IllegalArgumentException("The lines array is empty."); }
+
+    int[] result = new int[lines.get(0).length()];
+    for (String line : lines) {
+      for (int i = 0; i < line.length(); i++) {
+        char c = line.charAt(i);
+        // If `c` is 1, increment the count. Else, decrement.
+        result[i] += (c == '1' ? 1 : -1);
+      }
+    }
+
+    return result;
+  }
+}
