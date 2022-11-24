be9f2757c3a440124b88ee459afec9ef9fef251d
Umair Saeed <umair.saeed@gmail.com>
2022-11-24 07:58:07 -0800

    [AoC][Java][2021] Day03, part 2.

--- Java/AoC-Java/src/com/umair/aoc/y2021/Day03.java
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day03.java
@@ -21,7 +21,7 @@ public class Day03 extends Day {
       return Constants.INPUT_EMPTY;
     }
 
-    int[] countsOfOneDigit = countsOneBitPerPosition(lines);
+    int[] countsOfOneDigit = netCountOfOnesAtEachIndex(lines);
     long gamma = 0;
     long epsilon = 0;
 
@@ -44,9 +44,59 @@ public class Day03 extends Day {
     return Long.toString(powerConsumption);
   }
 
+  /**
+   * Finds the life support rating.
+   */
   @Override
   protected String part2(List<String> lines) {
-    return null;
+    if (lines.isEmpty()) {
+      return Constants.INPUT_EMPTY;
+    }
+
+    // Part-1: Compute the oxygen generator rating.
+    // To find oxygen generator rating, determine the most common value (0 or 1) in the current
+    // bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally
+    // common, keep values with a 1 in the position being considered.
+    List<String> filteredLines = lines;
+    int index = 0; // Start with the most significant digit.
+
+    while (filteredLines.size() > 1) {
+      int[] countsOfOneDigit = netCountOfOnesAtEachIndex(filteredLines);
+      char mostCommonDigit = countsOfOneDigit[index] >= 0 ? '1' : '0';
+      int idx = index; // Needed to use the index within the lambda.
+      filteredLines = filteredLines
+          .stream()
+          .filter(l -> l.charAt(idx) == mostCommonDigit)
+          .collect(Collectors.toList());
+
+      index += 1;
+    }
+    long oxygenGeneratorRating = Long.parseLong(filteredLines.get(0), 2);
+
+    // Part-2: Compute the CO2 scrubber rating.
+    // To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit
+    // position, and keep only numbers with that bit in that position. If 0 and 1 are equally
+    // common, keep values with a 0 in the position being considered.
+    filteredLines = lines;
+    // Start with the most significant digit.
+    index = 0;
+
+    while (filteredLines.size() > 1) {
+      int[] countsOfOneDigit = netCountOfOnesAtEachIndex(filteredLines);
+      char leastCommonDigit = countsOfOneDigit[index] >= 0 ? '0' : '1';
+
+      int idx = index; // Needed to use the index within the lambda.
+      filteredLines = filteredLines
+          .stream()
+          .filter(l -> l.charAt(idx) == leastCommonDigit)
+          .collect(Collectors.toList());
+      index += 1;
+    }
+    long cO2ScrubberRating = Long.parseLong(filteredLines.get(0), 2);
+
+    // Part 3: Calculate the life support rating.
+    long lifeSupportRating = oxygenGeneratorRating * cO2ScrubberRating;
+    return Long.toString(lifeSupportRating);
   }
 
   @Override
@@ -56,7 +106,7 @@ public class Day03 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
   }
 
   /**
@@ -65,7 +115,7 @@ public class Day03 extends Day {
    * <p></p>
    * Assumes: each element in `lines` is the same width.
    */
-  private static int[] countsOneBitPerPosition(List<String> lines) {
+  private static int[] netCountOfOnesAtEachIndex(List<String> lines) {
     if (lines.isEmpty()) { throw new IllegalArgumentException("The lines array is empty."); }
 
     int[] result = new int[lines.get(0).length()];
