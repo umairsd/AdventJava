4b4d79dc8c2246e4a44b4c2c166729f8468cb826
Umair Saeed <umair.saeed@gmail.com>
2022-11-24 16:34:47 -0800

    [AoC][Java][2021] Day 05, part 2.

--- Java/AoC-Java/src/com/umair/aoc/y2021/Day05.java
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day05.java
@@ -10,6 +10,9 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class Day05 extends Day {
 
   public Day05() {
@@ -43,7 +46,49 @@ public class Day05 extends Day {
 
   @Override
   protected String part2(List<String> data) {
-    return null;
+    List<Line> lines = buildLines(data);
+    int maxDimension = getMaxDimension(lines);
+    int[][] grid = new int[maxDimension + 1][maxDimension + 1];
+
+    lines.forEach(ln -> {
+      Point start = ln.start;
+      Point end = ln.end;
+
+      if (start.x == end.x) { // x is column, so this is a vertical line.
+        for (int row = min(start.y, end.y); row <= max(start.y, end.y); row++) {
+          grid[row][start.x] += 1;
+        }
+
+      } else if (start.y == end.y) { // y is row, so this is a horizontal line.
+        for (int column = min(start.x, end.x); column <= max(start.x, end.x); column++) {
+          grid[start.y][column] += 1;
+        }
+
+      } else if ((start.x < end.x && start.y < end.y) || // diagonal from top-left to bottom-right.
+                 (end.x < start.x && end.y < start.y)) { // diagonal from bottom-right to top-left.
+        int row = min(start.y, end.y);
+        int column = min(start.x, end.x);
+
+        while (row <= max(start.y, end.y) && column <= max(start.x, end.x)) {
+          grid[row][column] += 1;
+          row++;
+          column++;
+        }
+
+      } else { // diagonal from bottom-left to top-right, OR top-right to bottom-left
+        int row = max(start.y, end.y);
+        int column = min(start.x, end.x);
+
+        while (row >= min(start.y, end.y) && column <= max(start.x, end.x)) {
+          grid[row][column] += 1;
+          row--;
+          column++;
+        }
+      }
+    });
+
+    int dangerousAreas = countDangerousAreas(grid);
+    return Integer.toString(dangerousAreas);
   }
 
   @Override
@@ -53,7 +98,7 @@ public class Day05 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
   }
 
   private static int countDangerousAreas(int[][] grid) {
