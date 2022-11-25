17ee6dce9b042de67ce4c379b442eb4c210fde84
Umair Saeed <umair.saeed@gmail.com>
2022-11-25 13:01:46 -0800

    [AoC][Java][2021] Day 06, part 2

--- Java/AoC-Java/src/com/umair/aoc/y2021/Day06.java
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day06.java
@@ -2,13 +2,13 @@ package com.umair.aoc.y2021;
 
 import com.umair.aoc.common.Day;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day06 extends Day {
+
+  private static final int TIME_TO_SPAWN_PARENT = 6;
+  private static final int TIME_TO_SPAWN_CHILD = 8;
 
   public Day06() {
     super(6, 2021);
@@ -34,13 +34,51 @@ public class Day06 extends Day {
       iterationCount++;
     }
 
-    int numFish = startingFish.size();
-    return Integer.toString(numFish);
+    long numFish = startingFish.size();
+    return Long.toString(numFish);
   }
 
   @Override
   protected String part2(List<String> lines) {
-    return null;
+    List<Lanternfish> startingFish = Arrays.stream(lines.get(0).strip().split(","))
+        .map(String::strip)
+        .map(Integer::parseInt)
+        .map(Lanternfish::new)
+        .collect(Collectors.toCollection(ArrayList::new)); // creates a mutable collection.
+
+    // For each generation, store the count of fish created, and their current timer.
+    List<Generation> spawnedGenerations = new ArrayList<>();
+    long totalFish = startingFish.size();
+
+    int iterationCount = 0;
+    while (iterationCount < 256) {
+      long spawnedByOriginal = startingFish
+          .stream()
+          .map(Lanternfish::update)
+          .flatMap(Optional::stream)
+          .count();
+
+      long spawned = 0;
+
+      for (Generation gen : spawnedGenerations) {
+        // Find any generations where the timer is 0.
+        if (gen.timer == 0) {
+          // Reset the timer, and create new fish by adding to `spawned`.
+          gen.timer = TIME_TO_SPAWN_PARENT;
+          spawned += gen.fishCount;
+        } else {
+          gen.timer -= 1;
+        }
+      }
+
+      long totalSpawned = spawnedByOriginal + spawned;
+      spawnedGenerations.add(new Generation(totalSpawned, TIME_TO_SPAWN_CHILD));
+
+      totalFish += totalSpawned;
+      iterationCount++;
+    }
+
+    return Long.toString(totalFish);
   }
 
   @Override
@@ -50,12 +88,10 @@ public class Day06 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
   }
 
   private static class Lanternfish {
-    private static final int PARENT_DEFAULT_SPAWN_TIME = 6;
-    private static final int CHILD_SPAWN_TIME = 8;
     private int timer;
 
     private Lanternfish(int timer) {
@@ -71,13 +107,22 @@ public class Day06 extends Day {
       // If the current value is zero, then the timer resets, and a new
       // Lanternfish is spawned.
       if (timer == 0) {
-        timer = PARENT_DEFAULT_SPAWN_TIME;
-        Lanternfish spawn = new Lanternfish(CHILD_SPAWN_TIME);
+        timer = TIME_TO_SPAWN_PARENT;
+        Lanternfish spawn = new Lanternfish(TIME_TO_SPAWN_CHILD);
         return Optional.of(spawn);
       } else {
         timer--;
         return Optional.empty();
       }
+    }
+  }
+
+  private static class Generation {
+    long fishCount;
+    long timer;
+    Generation(long fishCount, long timer) {
+      this.fishCount = fishCount;
+      this.timer = timer;
     }
   }
 }
