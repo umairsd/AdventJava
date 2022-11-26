70f9f490cbd1dc84343ef67e63736d1291ae991f
Umair Saeed <umair.saeed@gmail.com>
2022-11-26 06:49:21 -0800

    [AoC][Java][2021] Day 09, part 2.

--- Java/AoC-Java/src/com/umair/aoc/common/Constants.java
+++ Java/AoC-Java/src/com/umair/aoc/common/Constants.java
@@ -2,7 +2,7 @@ package com.umair.aoc.common;
 
 public class Constants {
 
-  public static final String NOT_IMPLEMENTED = "Not implemented.";
+  public static final String ERROR = "Error processing the data.";
 
   public static final String INPUT_EMPTY = "Input is empty.";
 }
--- Java/AoC-Java/src/com/umair/aoc/y2021/Day09.java
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day09.java
@@ -1,9 +1,13 @@
 package com.umair.aoc.y2021;
 
-import com.umair.aoc.common.Constants;
 import com.umair.aoc.common.Day;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static com.umair.aoc.common.Constants.ERROR;
+import static com.umair.aoc.common.Constants.INPUT_EMPTY;
 
 public class Day09 extends Day {
 
@@ -14,7 +18,7 @@ public class Day09 extends Day {
   @Override
   protected String part1(List<String> lines) {
     if (lines.isEmpty()) {
-      return Constants.INPUT_EMPTY;
+      return INPUT_EMPTY;
     }
     int rowsCount = lines.size();
     int columnCount = lines.get(0).length();
@@ -28,14 +32,8 @@ public class Day09 extends Day {
 
     for (int row = 0; row < rowsCount; row++) {
       for (int column = 0; column < columnCount; column++) {
-        int currentHeight = grid[row][column];
-
-        if (currentHeight < safeGridValue(grid, row - 1, column) &&
-            currentHeight < safeGridValue(grid, row + 1, column) &&
-            currentHeight < safeGridValue(grid, row, column - 1) &&
-            currentHeight < safeGridValue(grid, row, column + 1)) {
-
-          totalRiskLevel += (currentHeight + 1);
+        if (isLowPoint(grid, row, column)) {
+          totalRiskLevel += (grid[row][column] + 1);
         }
       }
     }
@@ -45,7 +43,37 @@ public class Day09 extends Day {
 
   @Override
   protected String part2(List<String> lines) {
-    return null;
+    if (lines.isEmpty()) {
+      return INPUT_EMPTY;
+    }
+    int rowsCount = lines.size();
+    int columnCount = lines.get(0).length();
+
+    int[][] grid = new int[rowsCount][columnCount];
+    for (int row = 0; row < lines.size(); row++) {
+      grid[row] = lines.get(row).chars().map(Character::getNumericValue).toArray();
+    }
+
+    boolean[][] visited = new boolean[rowsCount][columnCount];
+    List<Integer> basinSizes = new ArrayList<>();
+
+    for (int row = 0; row < rowsCount; row++) {
+      for (int column = 0; column < columnCount; column++) {
+        if (isLowPoint(grid, row, column)) {
+          // Start a DFS scan starting from the current lowest point.
+          int basinSize = dfsVisit(grid, visited, new Coordinate(row, column));
+          basinSizes.add(basinSize);
+        }
+      }
+    }
+
+    basinSizes.sort(Collections.reverseOrder());
+    if (basinSizes.size() < 3) {
+      return ERROR;
+    }
+
+    int threeLargestBasins = basinSizes.get(0) * basinSizes.get(1) * basinSizes.get(2);
+    return Integer.toString(threeLargestBasins);
   }
 
   @Override
@@ -55,7 +83,57 @@ public class Day09 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
+  }
+
+  private static int dfsVisit(int[][] grid, boolean[][] visited, Coordinate coordinate) {
+    assert(canVisit(grid, visited, coordinate));
+
+    visited[coordinate.row][coordinate.column] = true;
+    int basinSize = 1; // Visiting the current coordinate.
+
+    for (Coordinate neighbor : neighbors(coordinate)) {
+      if (canVisit(grid, visited, neighbor)) {
+        basinSize += dfsVisit(grid, visited, neighbor);
+      }
+    }
+
+    return basinSize;
+  }
+
+  private static boolean canVisit(int[][] grid, boolean[][] visited, Coordinate coordinate) {
+    int rowCount = grid.length;
+    int columnCount = grid[0].length;
+    if (coordinate.row < 0 || coordinate.row >= rowCount) {
+      return false;
+    }
+    if (coordinate.column < 0 || coordinate.column >= columnCount) {
+      return false;
+    }
+    if (visited[coordinate.row][coordinate.column]) {
+      return false;
+    }
+    if (grid[coordinate.row][coordinate.column] == 9) {
+      return false;
+    }
+    return true;
+  }
+
+  private static List<Coordinate> neighbors(Coordinate c) {
+    return List.of(
+        new Coordinate(c.row - 1, c.column),
+        new Coordinate(c.row + 1, c.column),
+        new Coordinate(c.row, c.column + 1),
+        new Coordinate(c.row, c.column - 1));
+  }
+
+  private static boolean isLowPoint(int[][] grid, int row, int column) {
+    int currentHeight = grid[row][column];
+
+    return currentHeight < safeGridValue(grid, row - 1, column) &&
+        currentHeight < safeGridValue(grid, row + 1, column) &&
+        currentHeight < safeGridValue(grid, row, column - 1) &&
+        currentHeight < safeGridValue(grid, row, column + 1);
   }
 
   private static int safeGridValue(int[][] grid, int row, int column) {
@@ -68,5 +146,14 @@ public class Day09 extends Day {
     }
 
     return grid[row][column];
+  }
+
+  private static class Coordinate {
+    int row;
+    int column;
+    Coordinate(int row, int column) {
+      this.row = row;
+      this.column = column;
+    }
   }
 }
