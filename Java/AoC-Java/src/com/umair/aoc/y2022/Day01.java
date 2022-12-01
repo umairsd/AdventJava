7d60f3c29b927d29b9672f4c491ce4a526576874
Umair Saeed <umair.saeed@gmail.com>
2022-12-01 06:39:33 -0800

    [AoC][Java][2022] Day 01, part 2 (Calorie Counting).

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day01.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day01.java
@@ -1,7 +1,9 @@
 package com.umair.aoc.y2022;
 
 import com.umair.aoc.common.Day;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 import static com.umair.aoc.common.Constants.INPUT_EMPTY;
 
@@ -11,6 +13,7 @@ public class Day01 extends Day {
     super(1, 2022);
   }
 
+  /** Find the elf carrying the most calories. */
   @Override
   protected String part1(List<String> lines) {
     if (lines.isEmpty()) { return INPUT_EMPTY; }
@@ -30,12 +33,25 @@ public class Day01 extends Day {
       currentTotal += calories;
     }
 
+    // Process the last line.
+    maxCalories = Math.max(maxCalories, currentTotal);
     return Long.toString(maxCalories);
   }
 
+  /** Find the top 3 elves carrying the most calories. */
   @Override
   protected String part2(List<String> lines) {
-    return null;
+    if (lines.isEmpty()) { return INPUT_EMPTY; }
+
+    PriorityQueue<Long> elfCalories = parseToPerElfCalories(lines);
+
+    assert(elfCalories.size() >= 3);
+    List<Long> result = Stream.generate(elfCalories::poll)
+        .limit(3)
+        .toList();
+    long topThree = result.get(0) + result.get(1) + result.get(2);
+
+    return Long.toString(topThree);
   }
 
   @Override
@@ -45,6 +61,28 @@ public class Day01 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
+  }
+
+  private static PriorityQueue<Long> parseToPerElfCalories(List<String> lines) {
+    PriorityQueue<Long> elfCalories = new PriorityQueue<>(Collections.reverseOrder());
+    long currentTotal = 0;
+
+    for (String line : lines) {
+      if (line.isEmpty()) {
+        elfCalories.offer(currentTotal);
+        // Reset state.
+        currentTotal = 0;
+        continue;
+      }
+      long calories = Long.parseLong(line);
+      currentTotal += calories;
+    }
+
+    if (currentTotal != 0) {
+      elfCalories.offer(currentTotal);
+    }
+
+    return elfCalories;
   }
 }
