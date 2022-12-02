df86089b167a3d0455391e3a32c54926b0d6596c
Umair Saeed <umair.saeed@gmail.com>
2022-12-02 11:37:21 -0800

    [AoC][Java][2022] Day 02, part 2.

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day02.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day02.java
@@ -1,7 +1,6 @@
 package com.umair.aoc.y2022;
 
 import com.umair.aoc.common.Day;
-
 import java.util.List;
 
 public class Day02 extends Day {
@@ -12,10 +11,10 @@ public class Day02 extends Day {
 
   @Override
   protected String part1(List<String> lines) {
-    List<GameRound> rounds = parseLinesToGameRounds(lines);
-    long totalScore = rounds
+    List<SpecificMoveStrategy> gameRounds = parseLinesToSpecificMoveStrategies(lines);
+    long totalScore = gameRounds
         .stream()
-        .map(GameRound::scoreForHuman)
+        .map(SpecificMoveStrategy::scoreForHuman)
         .reduce(0L, Long::sum);
 
     return Long.toString(totalScore);
@@ -23,7 +22,13 @@ public class Day02 extends Day {
 
   @Override
   protected String part2(List<String> lines) {
-    return null;
+    List<DesiredOutcomeStrategy> gameRounds = parseLinesToDesiredOutcomeStrategies(lines);
+    long totalScore = gameRounds
+        .stream()
+        .map(DesiredOutcomeStrategy::score)
+        .reduce(0L, Long::sum);
+
+    return Long.toString(totalScore);
   }
 
   @Override
@@ -33,39 +38,68 @@ public class Day02 extends Day {
 
   @Override
   protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+    return filenameFromDataFileNumber(2);
   }
 
-  private static List<GameRound> parseLinesToGameRounds(List<String> lines) {
-    List<GameRound> rounds = lines
+  private static List<SpecificMoveStrategy> parseLinesToSpecificMoveStrategies(List<String> lines) {
+    List<SpecificMoveStrategy> strategies = lines
         .stream()
         .map(line -> {
           String[] tokens = line.strip().split(" ");
-          assert(tokens.length == 2);
+          assert (tokens.length == 2);
 
-          GameMove elfMove = GameMove.stringToMove(tokens[0]);
-          GameMove ourMove = GameMove.stringToMove(tokens[1]);
-          GameRound round = new GameRound(elfMove, ourMove);
-          return round;
+          GameMove elfMove = GameMove.parseString(tokens[0]);
+          GameMove ourMove = GameMove.parseString(tokens[1]);
+          SpecificMoveStrategy strategy = new SpecificMoveStrategy(elfMove, ourMove);
+          return strategy;
         })
         .toList();
-    return rounds;
+    return strategies;
   }
 
-  private record GameRound(GameMove elfMove, GameMove humanMove) {
-    long scoreForHuman() {
-        GameOutcome outcome;
-        if (humanMove == elfMove) {
-          outcome = GameOutcome.DRAW;
-        } else if (elfMove.beatingMove() == humanMove) {
-          outcome = GameOutcome.WIN;
-        } else {
-          outcome = GameOutcome.LOSS;
-        }
+  private static List<DesiredOutcomeStrategy> parseLinesToDesiredOutcomeStrategies(
+      List<String> lines) {
+    List<DesiredOutcomeStrategy> strategies = lines
+        .stream()
+        .map(line -> {
+          String[] tokens = line.strip().split(" ");
+          assert (tokens.length == 2);
 
-        return outcome.points + humanMove.points;
-      }
+          GameMove elfMove = GameMove.parseString(tokens[0]);
+          GameOutcome outcome = GameOutcome.parseString(tokens[1]);
+          DesiredOutcomeStrategy strategy = new DesiredOutcomeStrategy(elfMove, outcome);
+          return strategy;
+        })
+        .toList();
+    return strategies;
+  }
+
+  /** Represents the strategy of getting to the desired outcome as specified in the guide. */
+  private record DesiredOutcomeStrategy(GameMove elfMove, GameOutcome desiredOutcome) {
+    long score() {
+      return switch (desiredOutcome) {
+        case WIN -> desiredOutcome.points + elfMove.winningCountermove().points;
+        case LOSS -> desiredOutcome.points + elfMove.losingCountermove().points;
+        case DRAW -> desiredOutcome.points + elfMove.points;
+      };
     }
+  }
+
+  /** Represents the strategy of playing the exact move specified in the guide. */
+  private record SpecificMoveStrategy(GameMove elfMove, GameMove humanMove) {
+    long scoreForHuman() {
+      GameOutcome outcome;
+      if (humanMove == elfMove) {
+        outcome = GameOutcome.DRAW;
+      } else if (elfMove.winningCountermove() == humanMove) {
+        outcome = GameOutcome.WIN;
+      } else {
+        outcome = GameOutcome.LOSS;
+      }
+
+      return outcome.points + humanMove.points;
+    }
+  }
 
   private enum GameOutcome {
     WIN(6),
@@ -73,8 +107,18 @@ public class Day02 extends Day {
     LOSS(0);
 
     private final int points;
+
     GameOutcome(int points) {
       this.points = points;
+    }
+
+    static GameOutcome parseString(String input) {
+      return switch (input) {
+        case "X" -> LOSS;
+        case "Y" -> DRAW;
+        case "Z" -> WIN;
+        default -> throw new IllegalStateException("Unexpected value: " + input);
+      };
     }
   }
 
@@ -84,11 +128,12 @@ public class Day02 extends Day {
     SCISSORS(3);
 
     private final long points;
+
     GameMove(long points) {
       this.points = points;
     }
 
-    GameMove beatingMove() {
+    GameMove winningCountermove() {
       return switch (this) {
         case ROCK -> PAPER;
         case PAPER -> SCISSORS;
@@ -96,11 +141,19 @@ public class Day02 extends Day {
       };
     }
 
-    static GameMove stringToMove(String input) {
+    GameMove losingCountermove() {
+      return switch (this) {
+        case ROCK -> SCISSORS;
+        case PAPER -> ROCK;
+        case SCISSORS -> PAPER;
+      };
+    }
+
+    static GameMove parseString(String input) {
       return switch (input) {
-        case "A", "X" -> GameMove.ROCK;
-        case "B", "Y" -> GameMove.PAPER;
-        case "C", "Z" -> GameMove.SCISSORS;
+        case "A", "X" -> ROCK;
+        case "B", "Y" -> PAPER;
+        case "C", "Z" -> SCISSORS;
         default -> throw new IllegalStateException("Unexpected value: " + input);
       };
     }
