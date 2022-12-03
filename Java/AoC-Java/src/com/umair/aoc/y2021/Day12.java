1d38587caf461a04aba3df8a8c51787b426b20a6
Umair Saeed <umair.saeed@gmail.com>
2022-12-02 18:27:18 -0800

    [Java][AoC][2021] Day 12, skeleton.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day12.java
@@ -0,0 +1,32 @@
+package com.umair.aoc.y2021;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+public class Day12 extends Day {
+
+  public Day12() {
+    super(12, 2021);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return filenameFromDataFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return filenameFromDataFileNumber(1);
+  }
+}
