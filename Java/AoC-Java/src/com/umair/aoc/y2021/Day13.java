811a888bcd9961ac7ed5bf892ab62a45c36e8e4b
Umair Saeed <umair.saeed@gmail.com>
2022-12-04 14:03:00 -0800

    [AoC][Java][2021] Day 13, part 1. Optimized.
    Instead of creating a new char[][] array everytime, simply update the dimensions of a Paper abstraction.

--- Java/AoC-Java/src/com/umair/aoc/y2021/Day13.java
+++ Java/AoC-Java/src/com/umair/aoc/y2021/Day13.java
@@ -2,7 +2,6 @@ package com.umair.aoc.y2021;
 
 import com.umair.aoc.common.Day;
 
-import java.awt.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -21,11 +20,9 @@ public class Day13 extends Day {
     Instruction firstInstruction =
         parseInstructions(lines.subList(splitIndex + 1, lines.size())).get(0);
 
-    char[][] paper = buildDottedPaper(points);
-
-    char[][] foldedPaper = foldPaper(paper, firstInstruction);
-
-    int dots = countDots(foldedPaper);
+    Paper paper = buildDottedPaper(points);
+    foldPaper(paper, firstInstruction);
+    int dots = countDots(paper);
     return Integer.toString(dots);
   }
 
@@ -44,110 +41,90 @@ public class Day13 extends Day {
     return filenameFromDataFileNumber(1);
   }
 
-  private static char[][] foldPaper(char[][] paper, Instruction instruction) {
-    return switch(instruction.foldDirection) {
+  private static void foldPaper(Paper paper, Instruction instruction) {
+    switch(instruction.foldDirection) {
       case UP -> foldUp(paper, instruction.foldPoint);
       case LEFT -> foldDown(paper, instruction.foldPoint);
-    };
+    }
   }
 
-  private static char[][] foldUp(char[][] paper, int yFoldPoint) {
-    int paperLength = paper.length;
-    int paperWidth = paper[0].length;
+  private static void foldUp(Paper paper, int yFoldPoint) {
+    int lengthOfBottom = paper.effectiveLength - yFoldPoint - 1;
+    int numRowsToFold = Math.min(yFoldPoint, lengthOfBottom);
 
-    int lengthOfBottom = paperLength - yFoldPoint - 1;
-    // yFoldPoint is the length of the top half, and lengthToFold is the length of the bottom half.
-    int lengthToFold = Math.min(yFoldPoint, lengthOfBottom);
+    for (int r = 1; r <= numRowsToFold; r++) {
+      int sourceRow = yFoldPoint + r;
+      int destinationRow = yFoldPoint - r;
 
-    for (int rowToFold = 1; rowToFold <= lengthToFold; rowToFold++) {
-      int source = yFoldPoint + rowToFold;
-      int rowToUpdate = yFoldPoint - rowToFold;
-
-      for (int column = 0; column < paperWidth; column++) {
-        if (paper[source][column] == DOT) {
-          paper[rowToUpdate][column] = paper[source][column];
+      for (int column = 0; column < paper.effectiveWidth; column++) {
+        if (paper.contents[sourceRow][column] == DOT) {
+          paper.contents[destinationRow][column] = paper.contents[sourceRow][column];
         }
       }
     }
 
-    // Build a new foldedPaper.
-    char[][] foldedPaper = new char[yFoldPoint][paperWidth];
-    for (int row = 0; row < foldedPaper.length; row++) {
-      for (int column = 0; column < foldedPaper[row].length; column++) {
-        foldedPaper[row][column] = paper[row][column];
-      }
-    }
-
-    return foldedPaper;
+    // Update the dimensions of the paper.
+    paper.setEffectiveLength(yFoldPoint);
   }
 
-  private static char[][] foldDown(char[][] paper, int xFoldPoint) {
-    int paperLength = paper.length;
-    int paperWidth = paper[0].length;
-
-    int widthOfRight = paperWidth - xFoldPoint - 1;
+  private static void foldDown(Paper paper, int xFoldPoint) {
+    int widthOfRight = paper.effectiveWidth - xFoldPoint - 1;
     int numColumnsToFold = Math.min(xFoldPoint, widthOfRight);
 
     for (int col = 1; col <= numColumnsToFold; col++) {
-      int source = xFoldPoint + col;
-      int columnToUpdate = xFoldPoint - col;
+      int sourceColumn = xFoldPoint + col;
+      int destinationColumn = xFoldPoint - col;
 
-      for (int row = 0; row < paperLength; row++) {
-        if (paper[row][source] == DOT) {
-          paper[row][columnToUpdate] = paper[row][source];
+      for (int row = 0; row < paper.effectiveLength; row++) {
+        if (paper.contents[row][sourceColumn] == DOT) {
+          paper.contents[row][destinationColumn] = paper.contents[row][sourceColumn];
         }
       }
     }
 
-    // Build a new foldedPaper.
-    char[][] foldedPaper = new char[paperLength][xFoldPoint];
-    for (int row = 0; row < foldedPaper.length; row++) {
-      for (int column = 0; column < foldedPaper[row].length; column++) {
-        foldedPaper[row][column] = paper[row][column];
-      }
-    }
-
-    return foldedPaper;
+    // Update dimensions.
+    paper.setEffectiveWidth(xFoldPoint);
   }
 
-  private static int countDots(char[][] dottedPaper) {
+  private static int countDots(Paper paper) {
     int dots = 0;
-    for (char[] line : dottedPaper) {
-      for (char c : line) {
+    for (int row = 0; row < paper.effectiveLength; row++) {
+      for (int column = 0; column < paper.effectiveWidth; column++) {
+        char c = paper.contents[row][column];
         dots += (c == DOT ? 1 : 0);
       }
     }
     return dots;
   }
 
-  private static char[][] buildDottedPaper(List<Point> points) {
+  private static Paper buildDottedPaper(List<Point> points) {
     int maxX = points.stream()
-        .map(Point::getX)
+        .map(Point::x)
         .mapToInt(i -> i)
         .max()
         .orElseThrow(NoSuchElementException::new);
 
     int maxY = points.stream()
-        .map(Point::getY)
+        .map(Point::y)
         .mapToInt(i -> i)
         .max()
         .orElseThrow(NoSuchElementException::new);
 
-    char[][] dottedPaper = new char[maxY + 1][maxX + 1];
+    char[][] paperContents = new char[maxY + 1][maxX + 1];
 
     // For easier debugging, initialize the paper with blanks.
     for (int r = 0; r <= maxY; r++) {
       for (int c = 0; c <= maxX; c++) {
-        dottedPaper[r][c] = '.';
+        paperContents[r][c] = '.';
       }
     }
 
     // Draw the dots.
     for (Point p : points) {
-      dottedPaper[p.y][p.x] = DOT;
+      paperContents[p.y][p.x] = DOT;
     }
 
-    return dottedPaper;
+    return new Paper(paperContents);
   }
 
   private static List<Point> parsePoints(List<String> lines) {
@@ -182,20 +159,7 @@ public class Day13 extends Day {
     return instruction;
   }
 
-  private static class Point {
-    private final int x;
-    private final int y;
-
-    Point(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-    int getX() {
-      return x;
-    }
-    int getY() {
-      return y;
-    }
+  private record Point(int x, int y) {
   }
 
   private static class Instruction {
@@ -217,6 +181,31 @@ public class Day13 extends Day {
         case "y" -> UP;
         default -> throw new IllegalStateException();
       };
+    }
+  }
+
+  private static class Paper {
+    private final char[][] contents;
+
+    private int effectiveWidth;
+    private int effectiveLength;
+
+    Paper(char[][] contents) {
+      this.contents = contents;
+      this.effectiveLength = contents.length;
+      this.effectiveWidth = contents[0].length;
+    }
+    void setEffectiveLength(int updatedLength) {
+      if (updatedLength >= contents.length || updatedLength < 0) {
+        throw new IllegalArgumentException();
+      }
+      this.effectiveLength = updatedLength;
+    }
+    void setEffectiveWidth(int updatedWidth) {
+      if (updatedWidth >= contents[0].length || updatedWidth < 0) {
+        throw new IllegalArgumentException();
+      }
+      this.effectiveWidth = updatedWidth;
     }
   }
 }
