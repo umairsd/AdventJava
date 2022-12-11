721aa06176bc309704c932e727cf55980685ce72
Umair Saeed <umair.saeed@gmail.com>
2022-12-11 12:49:32 -0800

    [AoC][Java][2022] Day 10, Streamline.

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day10.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day10.java
@@ -21,37 +21,10 @@ public class Day10 extends Day {
   @Override
   protected String part1(List<String> lines) {
     Set<Integer> checkpoints = new HashSet<>(List.of(20, 60, 100, 140, 180, 220));
-    // Each index contains the value of the register DURING that index.
-    int[] registerHistory = new int[MAX_CYCLES];
-    int registerX = 1;
-    // The next cycle that will end.
-    int cycle = 1;
-
-    for (String line : lines) {
-      if (cycle >= MAX_CYCLES - 1) {
-        break;
-      }
-
-      if (line.strip().equals("noop")) {
-        // Write the current value of X at the end of the cycle.
-        registerHistory[cycle++] = registerX;
-      } else {
-        String[] tokens = line.split(" ");
-        int value = Integer.parseInt(tokens[1].strip());
-        registerHistory[cycle++] = registerX;
-        registerHistory[cycle++] = registerX;
-
-        // Update the register.
-        registerX += value;
-      }
-    }
-
-    // If there are no more instructions, yet we still have cycles left, fill out the value of X.
-    Arrays.fill(registerHistory, cycle, registerHistory.length, registerX);
-
+    int[] registerHistory = parseRegisterHistoryAtCycleEnd(lines);
     long signalStrength = checkpoints
         .stream()
-        .reduce(0, (subTotal, e) -> subTotal + registerHistory[e] * e);
+        .reduce(0, (subTotal, e) -> subTotal + registerHistory[e - 1] * e);
     return Long.toString(signalStrength);
   }
 
