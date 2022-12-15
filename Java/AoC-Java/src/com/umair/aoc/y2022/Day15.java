e67050c860c200b07729d8a2950ef41beb9c6e16
Umair Saeed <umair.saeed@gmail.com>
2022-12-15 15:01:52 -0800

    [AoC][Java][2022] Day 15 stubs.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day15.java
@@ -0,0 +1,32 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+public class Day15 extends Day {
+
+  public Day15() {
+    super(15, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return filenameFromDataFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return filenameFromDataFileNumber(1);
+  }
+}
