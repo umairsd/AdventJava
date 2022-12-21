681ce6aa200ee1b2195d4ca9695eea2c0899158f
Umair Saeed <umair.saeed@gmail.com>
2022-12-21 15:12:52 -0800

    [AoC][Java][2022] Day 18, part 2.

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day18.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day18.java
@@ -2,7 +2,7 @@ package com.umair.aoc.y2022;
 
 import com.umair.aoc.common.Day;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +15,44 @@ public class Day18 extends Day {
   @Override
   protected String part1(List<String> lines) {
     List<Cube> cubes = lines.stream().map(Day18::parseCube).toList();
+    int allExposedSides = countExposedSides(cubes);
+    return Integer.toString(allExposedSides);
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    List<Cube> lavaCubes = lines.stream().map(Day18::parseCube).toList();
+    Box box = buildBox(lavaCubes);
+    // Set the state of all lava cubes.
+    lavaCubes.forEach(c -> box.setCubeState(c, CubeState.LAVA));
+    // Start filling the box with water. Uses Flood-Fill
+    floodFillWithWater(box);
+
+    // Find all cubes whose state is AIR. These are the inner cubes that were not filled with
+    // water during the previous step.
+    List<Cube> unfilledInnerCubes = box.cubeStateMap.entrySet().stream()
+        .filter(e -> e.getValue() == CubeState.AIR)
+        .map(Map.Entry::getKey)
+        .toList();
+
+
+    int allExposedSides = countExposedSides(lavaCubes);
+    int innerExposedSides = countExposedSides(unfilledInnerCubes);
+    int outerExposedSides = allExposedSides - innerExposedSides;
+    return Integer.toString(outerExposedSides);
+  }
+
+  @Override
+  protected String part1Filename() {
+    return filenameFromDataFileNumber(2);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return filenameFromDataFileNumber(2);
+  }
+
+  private static int countExposedSides(List<Cube> cubes) {
     int totalExposedSides = cubes.size() * 6;
 
     for (int i = 0; i < cubes.size(); i++) {
@@ -26,23 +64,30 @@ public class Day18 extends Day {
         }
       }
     }
-
-    return Integer.toString(totalExposedSides);
+    return totalExposedSides;
   }
 
-  @Override
-  protected String part2(List<String> lines) {
-    return null;
-  }
+  private static void floodFillWithWater(Box box) {
+    Queue<Cube> queue = new ArrayDeque<>();
+    queue.add(box.min);
+    // Set of cubes that have been added to the queue.
+    Set<Cube> processed = new HashSet<>();
+    processed.add(box.min);
 
-  @Override
-  protected String part1Filename() {
-    return filenameFromDataFileNumber(2);
-  }
+    while (!queue.isEmpty()) {
+      Cube c = queue.poll();
+      assert(box.getCubeState(c) == CubeState.AIR);
+      // Fill the cube with water.
+      box.setCubeState(c, CubeState.WATER);
 
-  @Override
-  protected String part2Filename() {
-    return filenameFromDataFileNumber(1);
+      List<Cube> neighbors = box.neighborsInside(c);
+      for (Cube n : neighbors) {
+        if (box.getCubeState(n) == CubeState.AIR && !processed.contains(n)) {
+          queue.add(n);
+          processed.add(n);
+        }
+      }
+    }
   }
 
   private static boolean areCubesTouching(Cube c1, Cube c2) {
@@ -65,5 +110,82 @@ public class Day18 extends Day {
     return new Cube(x, y, z);
   }
 
+  private static Box buildBox(List<Cube> cubes) {
+    int minX = cubes.stream().mapToInt(Cube::x).min().orElseThrow() - 1;
+    int minY = cubes.stream().mapToInt(Cube::y).min().orElseThrow() - 1;
+    int minZ = cubes.stream().mapToInt(Cube::z).min().orElseThrow() - 1;
+    Cube minCube = new Cube(minX, minY, minZ);
+
+    int maxX = cubes.stream().mapToInt(Cube::x).max().orElseThrow() + 1;
+    int maxY = cubes.stream().mapToInt(Cube::y).max().orElseThrow() + 1;
+    int maxZ = cubes.stream().mapToInt(Cube::z).max().orElseThrow() + 1;
+    Cube maxCube = new Cube(maxX, maxY, maxZ);
+
+    Box box = new Box(minCube, maxCube);
+    return box;
+  }
+
   private record Cube(int x, int y, int z) {}
+
+  private enum CubeState {
+    LAVA,
+    AIR,
+    WATER
+  }
+
+  private static class Box {
+    Cube min;
+    Cube max;
+    Map<Cube, CubeState> cubeStateMap = new HashMap<>();
+
+    private Box(Cube min, Cube max) {
+      this.min = min;
+      this.max = max;
+      fillBoxWith(this, CubeState.AIR);
+    }
+
+    private void setCubeState(Cube cube, CubeState state) {
+      cubeStateMap.put(cube, state);
+    }
+
+    private CubeState getCubeState(Cube cube) {
+      if (!cubeStateMap.containsKey(cube)) {
+        throw new IllegalStateException("Cube is not inside the box: " + cube);
+      }
+      return cubeStateMap.get(cube);
+    }
+
+    private boolean isInside(Cube cube) {
+      boolean validX = (cube.x >= min.x && cube.x <= max.x);
+      boolean validY = (cube.y >= min.y && cube.y <= max.y);
+      boolean validZ = (cube.z >= min.z && cube.z <= max.z);
+      return validX && validY && validZ;
+    }
+
+    private List<Cube> neighborsInside(Cube cube) {
+      List<Cube> neighbors =  List.of(
+          new Cube(cube.x, cube.y, cube.z + 1),
+          new Cube(cube.x, cube.y, cube.z - 1),
+          new Cube(cube.x, cube.y + 1, cube.z),
+          new Cube(cube.x, cube.y - 1, cube.z),
+          new Cube(cube.x + 1, cube.y, cube.z),
+          new Cube(cube.x - 1, cube.y, cube.z)
+      );
+
+      List<Cube> filtered = neighbors.stream()
+          .filter(this::isInside)
+          .toList();
+      return filtered;
+    }
+
+    private static void fillBoxWith(Box box, CubeState state) {
+      for (int x = box.min.x; x <= box.max.x; x++) {
+        for (int y = box.min.y; y <= box.max.y; y++) {
+          for (int z = box.min.z; z <= box.max.z; z++) {
+            box.setCubeState(new Cube(x, y, z), state);
+          }
+        }
+      }
+    }
+  }
 }
