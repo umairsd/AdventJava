1473009dae907995a3af63613907558dc6c88eff
Umair Saeed <umair.saeed@gmail.com>
2022-12-21 16:38:21 -0800

    [AoC][Java][2022] Day 21, stubs.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day21.java
@@ -0,0 +1,36 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+/**
+ * Day 21: Monkey Math
+ * <a href="https://adventofcode.com/2022/day/21">2022, Day-21</a>
+ */
+public class Day21 extends Day {
+
+  public Day21() {
+    super(21, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return null;
+  }
+
+  @Override
+  protected String part2Filename() {
+    return null;
+  }
+}
