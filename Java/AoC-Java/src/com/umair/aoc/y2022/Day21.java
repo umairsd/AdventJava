58d5d0ebfc0264eb0663299fcc2fe9a7dfb2d195
Umair Saeed <umair.saeed@gmail.com>
2022-12-21 21:22:24 -0800

    [AoC][Java][2022] Day 21, part 1.

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day21.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day21.java
@@ -3,6 +3,11 @@ package com.umair.aoc.y2022;
 import com.umair.aoc.common.Day;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Day 21: Monkey Math
@@ -16,7 +21,9 @@ public class Day21 extends Day {
 
   @Override
   protected String part1(List<String> lines) {
-    return null;
+    Node root = parseTree(lines);
+    long result = preorderCompute(root);
+    return Long.toString(result);
   }
 
   @Override
@@ -26,11 +33,119 @@ public class Day21 extends Day {
 
   @Override
   protected String part1Filename() {
-    return null;
+    return filenameFromDataFileNumber(1);
   }
 
   @Override
   protected String part2Filename() {
-    return null;
+    return filenameFromDataFileNumber(1);
+  }
+
+  private static long preorderCompute(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    long leftValue = preorderCompute(root.left);
+    long rightValue = preorderCompute(root.right);
+    long result = switch (root.operation) {
+      case LITERAL -> root.value;
+      case ADD -> leftValue + rightValue;
+      case SUBTRACT -> leftValue - rightValue;
+      case MULTIPLY -> leftValue * rightValue;
+      case DIVIDE -> leftValue / rightValue;
+    };
+    return result;
+  }
+
+  private static Node parseTree(List<String> lines) {
+    Map<String, NodeData> dataMap = lines.stream()
+        .map(Day21::parseNodeData)
+        .collect(Collectors.toMap(NodeData::name, Function.identity()));
+
+    Node root = buildTree("root", dataMap);
+    return root;
+  }
+
+  private static Node buildTree(String nodeName, Map<String, NodeData> dataMap) {
+    if (nodeName == null) {
+      return null;
+    }
+    if (!dataMap.containsKey(nodeName)) {
+      throw new IllegalStateException("Node name not found in the data map: " + nodeName);
+    }
+
+    Node root = new Node(nodeName);
+    NodeData data = dataMap.get(nodeName);
+    if (data.value != null) {
+      // This is a leaf node.
+      root.value = Integer.parseInt(data.value);
+      root.operation = Operation.LITERAL;
+      return root;
+    }
+    root.operation = parseOperation(data.op);
+    root.left = buildTree(data.leftName, dataMap);
+    root.right = buildTree(data.rightName, dataMap);
+    return root;
+  }
+
+  private static Operation parseOperation(String s) {
+    if (s == null) { return Operation.LITERAL; }
+
+    return switch(s) {
+      case "+" -> Operation.ADD;
+      case "-" -> Operation.SUBTRACT;
+      case "*" -> Operation.MULTIPLY;
+      case "/" -> Operation.DIVIDE;
+      default -> throw new IllegalArgumentException("Cannot parse operation for " + s);
+    };
+  }
+
+  private static final Pattern nodePattern = Pattern.compile("(.*): (.*) (.*) (.*)");
+  private static final Pattern leafPattern = Pattern.compile("(.*): (.*)");
+
+  private static NodeData parseNodeData(String line) {
+    Matcher leafMatcher = leafPattern.matcher(line);
+    Matcher nodeMatcher = nodePattern.matcher(line);
+
+    if (nodeMatcher.matches()) {
+      var name = nodeMatcher.group(1);
+      var leftName = nodeMatcher.group(2);
+      var operation = nodeMatcher.group(3);
+      var rightName = nodeMatcher.group(4);
+      NodeData node = new NodeData(name, leftName, operation, null, rightName);
+      return node;
+
+    } else if (leafMatcher.matches()) {
+      var name = leafMatcher.group(1);
+      var value = leafMatcher.group(2);
+
+      NodeData node = new NodeData(name, null, null, value, null);
+      return node;
+
+    } else {
+      throw new IllegalStateException("Bad input line: " + line);
+    }
+  }
+
+  private record NodeData(String name, String leftName, String op, String value, String rightName) { }
+
+  private enum Operation {
+    LITERAL,
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
+  }
+
+  private static class Node {
+    String name;
+    Operation operation;
+    Node left;
+    Node right;
+    int value;
+
+    Node(String name) {
+      this.name = name;
+    }
   }
 }
