92c76d2612e28ceda7669f4aa81228331515cb79
Umair Saeed <umair.saeed@gmail.com>
2022-12-24 07:44:47 -0800

    [AoC][Java][2022] Day 22. Stubs

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day22.java
@@ -0,0 +1,32 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+public class Day22 extends Day {
+
+  public Day22() {
+    super(22, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return fileNameFromFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return fileNameFromFileNumber(1);
+  }
+}
---
+++ Java/AoC-Java/test/com/umair/aoc/y2022/Day22Test.java
@@ -0,0 +1,24 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.DayTestBase;
+import org.junit.jupiter.api.Test;
+
+class Day22Test extends DayTestBase {
+
+  @Test
+  void testPart1_Example() {
+    runPartForDayWithFileNumber(1, new Day22(), 1, "6032");
+  }
+
+  @Test
+  void testPart1_Full() {
+  }
+
+  @Test
+  void testPart2_Example() {
+  }
+
+  @Test
+  void testPart2_Full() {
+  }
+}
