76ddac48ed286d0be75c94a5e8f0c2d9850542e4
Umair Saeed <umair.saeed@gmail.com>
2022-12-26 15:02:10 -0800

    [AoC][Java][2022] Day 25.

--- Java/AoC-Java/src/com/umair/aoc/y2022/Day25.java
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day25.java
@@ -2,6 +2,7 @@ package com.umair.aoc.y2022;
 
 import com.umair.aoc.common.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +17,10 @@ public class Day25 extends Day {
 
   @Override
   protected String part1(List<String> lines) {
-    return null;
+    List<Long> snafus = lines.stream().map(Day25::snafuToDecimal).toList();
+    long decimalResult = snafus.stream().reduce(0L, Long::sum);
+    String snafuResult = decimalToSnafu(decimalResult);
+    return snafuResult;
   }
 
   @Override
@@ -26,11 +30,114 @@ public class Day25 extends Day {
 
   @Override
   protected String part1Filename() {
-    return fileNameFromFileNumber(1);
+    return fileNameFromFileNumber(2);
   }
 
   @Override
   protected String part2Filename() {
     return fileNameFromFileNumber(1);
+  }
+
+  public static long snafuToDecimal(String line) {
+    long number = 0;
+    for (int i = 0; i < line.length(); i++) {
+      int power = line.length() - i - 1;
+      char c = line.charAt(i);
+
+      long snafuDigit = snafuCharacterToDecimal(c);
+      number += (long)Math.pow(5, power) * snafuDigit;
+    }
+
+    return number;
+  }
+
+  public static String decimalToSnafu(long number) {
+    List<Long> base5LSBFirst = decimalToBaseFiveLSBFirst(number);
+    List<Character> snafuLSBFirst = base5LSBFirstToSnafuLSBFirst(base5LSBFirst);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = snafuLSBFirst.size() - 1; i >= 0; i--) {
+      sb.append(snafuLSBFirst.get(i));
+    }
+
+    return sb.toString();
+  }
+
+  private static List<Character> base5LSBFirstToSnafuLSBFirst(List<Long> base5LSBFirst) {
+    List<Character> result = new ArrayList<>();
+    long remainder = 0;
+    for (Long aLong : base5LSBFirst) {
+      long digit = aLong + remainder;
+
+      List<Integer> snafuDigits = base5DigitToSnafuDigits(digit);
+      if (snafuDigits.size() == 1) {
+        result.add(digitToSnafuCharacter(snafuDigits.get(0)));
+        remainder = 0;
+      } else {
+        result.add(digitToSnafuCharacter(snafuDigits.get(1)));
+        remainder = snafuDigits.get(0);
+      }
+    }
+
+    if (remainder != 0) {
+      result.add(digitToSnafuCharacter(remainder));
+    }
+
+    return result;
+  }
+
+  private static List<Long> decimalToBaseFiveLSBFirst(long number) {
+    if (number == 0) {
+      return List.of(0L);
+    }
+
+    List<Long> base5LSBFirst = new ArrayList<>();
+    while (number != 0) {
+      long remainder = number % 5;
+      number = number / 5;
+      base5LSBFirst.add(remainder);
+    }
+    return base5LSBFirst;
+  }
+
+  private static long snafuCharacterToDecimal(char c) {
+    return switch (c) {
+      case '=' -> -2;
+      case '-' -> -1;
+      case '0' -> 0;
+      case '1' -> 1;
+      case '2' -> 2;
+      default -> throw new IllegalStateException();
+    };
+  }
+
+  private static Character digitToSnafuCharacter(long digit) {
+    int value = (int)digit;
+    return switch(value) {
+      case -2 -> '=';
+      case -1 -> '-';
+      case 0 -> '0';
+      case 1 -> '1';
+      case 2 -> '2';
+      default -> throw new IllegalStateException("Invalid value for snafu digit: " + digit);
+    };
+  }
+
+  private static List<Integer> base5DigitToSnafuDigits(long base5Digit) {
+    int value = (int) base5Digit;
+    return switch(value) {
+      case 0 -> List.of(0);
+      case 1 -> List.of(1);
+      case 2 -> List.of(2);
+      case 3 -> List.of(1, -2);
+      case 4 -> List.of(1, -1);
+      case 5 -> List.of(1, 0);
+      case 6 -> List.of(1, 1);
+      case 7 -> List.of(1, 2);
+      case 8 -> List.of(2, -2);
+      case 9 -> List.of(2, -1);
+      default -> throw new IllegalStateException();
+    };
+
   }
 }
--- Java/AoC-Java/test/com/umair/aoc/y2022/Day25Test.java
+++ Java/AoC-Java/test/com/umair/aoc/y2022/Day25Test.java
@@ -3,25 +3,87 @@ package com.umair.aoc.y2022;
 import com.umair.aoc.common.DayTestBase;
 import org.junit.jupiter.api.Test;
 
+import static com.umair.aoc.y2022.Day25.decimalToSnafu;
+import static  com.umair.aoc.y2022.Day25.snafuToDecimal;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Day25Test extends DayTestBase {
 
   @Test
   void testPart1_Example() {
-    runPartForDayWithFileNumber(1, new Day25(), 1, "4890");
+    runPartForDayWithFileNumber(1, new Day25(), 1, "2=-1=0");
   }
 
   @Test
   void testPart1_Full() {
-
+    runPartForDayWithFileNumber(1, new Day25(), 2, "20=2-02-0---02=22=21");
   }
 
   @Test
   void testPart2_Example() {
-
   }
 
   @Test
   void testPart2_Full() {
+  }
 
+  @Test
+  void testSnafuToDecimal() {
+    assertEquals(1, snafuToDecimal("1"));
+    assertEquals(2, snafuToDecimal("2"));
+    assertEquals(0, snafuToDecimal("0"));
+
+    assertEquals(1747, snafuToDecimal("1=-0-2"));
+    assertEquals(906, snafuToDecimal("12111"));
+    assertEquals(198, snafuToDecimal("2=0="));
+    assertEquals(11, snafuToDecimal("21"));
+    assertEquals(201, snafuToDecimal("2=01"));
+    assertEquals(31, snafuToDecimal("111"));
+    assertEquals(1257, snafuToDecimal("20012"));
+    assertEquals(32, snafuToDecimal("112"));
+    assertEquals(353, snafuToDecimal("1=-1="));
+    assertEquals(107, snafuToDecimal("1-12"));
+    assertEquals(7, snafuToDecimal("12"));
+    assertEquals(3, snafuToDecimal("1="));
+    assertEquals(37, snafuToDecimal("122"));
+
+    assertEquals(2022, snafuToDecimal("1=11-2"));
+    assertEquals(12345, snafuToDecimal("1-0---0"));
+    assertEquals(314159265, snafuToDecimal("1121-1110-1=0"));
+  }
+
+  @Test
+  void testDecimalToDecimal() {
+    assertEquals("0", decimalToSnafu(0));
+
+    assertEquals("1", decimalToSnafu(1));
+    assertEquals("2", decimalToSnafu(2));
+    assertEquals("1=", decimalToSnafu(3));
+    assertEquals("1-", decimalToSnafu(4));
+    assertEquals("10", decimalToSnafu(5));
+    assertEquals("11", decimalToSnafu(6));
+    assertEquals("12", decimalToSnafu(7));
+    assertEquals("2=", decimalToSnafu(8));
+    assertEquals("2-", decimalToSnafu(9));
+    assertEquals("20", decimalToSnafu(10));
+    assertEquals("21", decimalToSnafu(11));
+
+    assertEquals("1=-0-2", decimalToSnafu(1747));
+    assertEquals("12111", decimalToSnafu(906));
+    assertEquals("2=0=", decimalToSnafu(198));
+    assertEquals("21", decimalToSnafu(11));
+    assertEquals("2=01", decimalToSnafu(201));
+    assertEquals("111", decimalToSnafu(31));
+    assertEquals("20012", decimalToSnafu(1257));
+    assertEquals("112", decimalToSnafu(32));
+    assertEquals("1=-1=", decimalToSnafu(353));
+    assertEquals("1-12", decimalToSnafu(107));
+    assertEquals("12", decimalToSnafu(7));
+    assertEquals("1=", decimalToSnafu(3));
+    assertEquals("122", decimalToSnafu(37));
+
+    assertEquals("1=11-2", decimalToSnafu(2022));
+    assertEquals("1-0---0", decimalToSnafu(12345));
+    assertEquals("1121-1110-1=0", decimalToSnafu(314159265));
   }
 }
