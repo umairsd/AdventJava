91ab6080c397559fe9352d297147cb2587b30ccc
Umair Saeed <umair.saeed@gmail.com>
2022-12-26 11:44:03 -0800

    [AoC][Java][2022] Day 25, stubs.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day25.java
@@ -0,0 +1,36 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+/**
+ * Day 25: Full of Hot Air
+ * <a href="https://adventofcode.com/2022/day/25">2022, Day-25</a>
+ */
+public class Day25 extends Day {
+
+  public Day25() {
+    super(25, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return fileNameFromFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return fileNameFromFileNumber(1);
+  }
+}
---
+++ Java/AoC-Java/test/com/umair/aoc/y2022/Day25Test.java
@@ -0,0 +1,27 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.DayTestBase;
+import org.junit.jupiter.api.Test;
+
+class Day25Test extends DayTestBase {
+
+  @Test
+  void testPart1_Example() {
+    runPartForDayWithFileNumber(1, new Day25(), 1, "4890");
+  }
+
+  @Test
+  void testPart1_Full() {
+
+  }
+
+  @Test
+  void testPart2_Example() {
+
+  }
+
+  @Test
+  void testPart2_Full() {
+
+  }
+}
