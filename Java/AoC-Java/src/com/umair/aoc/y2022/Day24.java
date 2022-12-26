a64b8dbe31c822876e395a6c8b8cf854353a26bf
Umair Saeed <umair.saeed@gmail.com>
2022-12-26 15:07:24 -0800

    [AoC][Java][2022] Day 24, stubs.

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day24.java
@@ -0,0 +1,36 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+/**
+ * Day 24: Blizzard Basin
+ * <a href="https://adventofcode.com/2022/day/24">2022, Day-24</a>
+ */
+public class Day24 extends Day {
+
+  public Day24() {
+    super(24, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return fileNameFromFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return fileNameFromFileNumber(1);
+  }
+}
