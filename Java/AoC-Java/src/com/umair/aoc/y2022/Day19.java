7d3cbbea0c94bd4001bc2043aa3749084b4c7321
Umair Saeed <umair.saeed@gmail.com>
2022-12-30 08:28:20 -0800

    [AoC][Java][2022] Day 19: Stubs

---
+++ Java/AoC-Java/src/com/umair/aoc/y2022/Day19.java
@@ -0,0 +1,36 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.Day;
+
+import java.util.List;
+
+/**
+ * Day 19: Not Enough Minerals
+ * <a href="https://adventofcode.com/2022/day/19">2022, Day-19</a>
+ */
+public class Day19 extends Day {
+
+  public Day19() {
+    super(19, 2022);
+  }
+
+  @Override
+  protected String part1(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part2(List<String> lines) {
+    return null;
+  }
+
+  @Override
+  protected String part1Filename() {
+    return fileNameFromFileNumber(1);
+  }
+
+  @Override
+  protected String part2Filename() {
+    return fileNameFromFileNumber(1);
+  }
+}
---
+++ Java/AoC-Java/test/com/umair/aoc/y2022/Day19Test.java
@@ -0,0 +1,29 @@
+package com.umair.aoc.y2022;
+
+import com.umair.aoc.common.DayTestBase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day19Test extends DayTestBase {
+
+  @Test
+  void testPart1_Example() {
+//    runPartForDayWithFileNumber(1, new Day19(), 1, "");
+  }
+
+  @Test
+  void testPart1_Full() {
+//    runPartForDayWithFileNumber(1, new Day19(), 2, "");
+  }
+
+  @Test
+  void testPart2_Example() {
+//    runPartForDayWithFileNumber(2, new Day19(), 1, "");
+  }
+
+  @Test
+  void testPart2_Full() {
+//    runPartForDayWithFileNumber(2, new Day19(), 2, "");
+  }
+}
