729e139de96c1cdf0c54da7246599c8f2a1d5cce
Umair Saeed <umair.saeed@gmail.com>
2023-02-16 10:43:54 -0800

    [AoC][Swift][2022] Day 06: Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2022/Y2022Day06.swift
@@ -0,0 +1,18 @@
+// Created on 2/16/23.
+
+import Foundation
+
+class Y2022Day06: Day {
+
+  var dayNumber: Int = 6
+  var year: Int = 2022
+  var dataFileNumber: Int = 2
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
---
+++ Swift/AoC-Swift/AoC-SwiftTests/2022/Y2022Day06Test.swift
@@ -0,0 +1,30 @@
+// Created on 2/16/23.
+
+import XCTest
+
+final class Y2022Day06Test: XCTestCase {
+
+  func testPart1_Example() throws {
+    let day = Y2022Day06(dataFileNumber: 1)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "7")
+  }
+
+  func testPart1_Full() throws {
+    let day = Y2022Day06(dataFileNumber: 2)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "1855")
+  }
+
+  func testPart2_Example() throws {
+    let day = Y2022Day06(dataFileNumber: 1)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part2(lines), "14")
+  }
+
+  func testPart2_Full() throws {
+    let day = Y2022Day06(dataFileNumber: 2)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part2(lines), "3256")
+  }
+}
