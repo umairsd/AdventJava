be1e0fa6f2811697fb65c5adb5064ac8a2c78e23
Umair Saeed <umair.saeed@gmail.com>
2023-03-21 14:47:35 -0700

    [AoC][Swift][2022] Day 07: Initial files and stubs.

---
+++ Swift/AoC-Swift/AoC-Swift/2022/Y2022Day07.swift
@@ -0,0 +1,22 @@
+// Created on 3/21/23.
+
+import Foundation
+
+class Y2022Day07: Day {
+
+  var dayNumber: Int = 7
+  var year: Int = 2022
+  var dataFileNumber: Int = 2
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
