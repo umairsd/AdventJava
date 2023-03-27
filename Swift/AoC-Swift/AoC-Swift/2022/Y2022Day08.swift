d3de77ca7c4e1aee88b53a2e14443cba2b31dd66
Umair Saeed <umair.saeed@gmail.com>
2023-03-27 10:05:53 -0700

    [AoC][Swift][2022] Day 08: Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2022/Y2022Day08.swift
@@ -0,0 +1,17 @@
+// Created on 3/27/23.
+
+import Foundation
+
+class Y2022Day08: Day {
+  var dayNumber: Int = 8
+  var year: Int = 2022
+  var dataFileNumber: Int = 2
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
