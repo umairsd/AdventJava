587149246bca60ed87af3b900eb519ebeabca7f5
Umair Saeed <umair.saeed@gmail.com>
2023-04-27 10:22:43 -0700

    [AoC][Swift][2022] Day 10: Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2022/Y2022Day10.swift
@@ -0,0 +1,22 @@
+// Created on 4/27/23.
+
+import Foundation
+
+/// --- Day 10: Cathode-Ray Tube ---
+class Y2022Day10: Day {
+  var dayNumber: Int = 10
+  var year: Int = 2022
+  var dataFileNumber: Int = 2
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
---
+++ Swift/AoC-Swift/AoC-SwiftTests/2022/Y2022Day10Test.swift
@@ -0,0 +1,47 @@
+// Created on 4/27/23.
+
+import XCTest
+
+final class Y2022Day10Test: XCTestCase {
+
+  func DISABLED_testPart1_Example() throws {
+    let day = Y2022Day10(dataFileNumber: 1)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "13140")
+  }
+
+  func DISABLED_testPart1_Full() throws {
+    let day = Y2022Day10(dataFileNumber: 2)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "16480")
+  }
+
+  func DISABLED_testPart2_Example() throws {
+    let day = Y2022Day10(dataFileNumber: 1)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+
+    let expected = """
+    ##..##..##..##..##..##..##..##..##..##..
+    ###...###...###...###...###...###...###.
+    ####....####....####....####....####....
+    #####.....#####.....#####.....#####.....
+    ######......######......######......####
+    #######.......#######.......#######.....
+    """
+    XCTAssertEqual(day.part2(lines), expected)
+  }
+
+  func DISABLED_testPart2_Full() throws {
+    let day = Y2022Day10(dataFileNumber: 2)
+    let lines = TestUtils().loadTestData(from: day.dataFilename())
+    let expected = """
+    ###..#....####.####.#..#.#....###..###..
+    #..#.#....#....#....#..#.#....#..#.#..#.
+    #..#.#....###..###..#..#.#....#..#.###..
+    ###..#....#....#....#..#.#....###..#..#.
+    #....#....#....#....#..#.#....#....#..#.
+    #....####.####.#.....##..####.#....###.
+    """
+    XCTAssertEqual(day.part2(lines), expected)
+  }
+}
