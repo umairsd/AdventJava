220ceeedf4941fe7f8ed879701fe99221cb4d8a9
Umair Saeed <umair.saeed@gmail.com>
2023-12-03 13:54:08 -0800

    [AoC][Swift][2023] Day 02: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day02.swift
@@ -0,0 +1,22 @@
+// Created on 12/3/23.
+
+import Foundation
+
+/// --- Day 2: Cube Conundrum ---
+class Y2023Day02: Day {
+  var dayNumber: Int = 2
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
