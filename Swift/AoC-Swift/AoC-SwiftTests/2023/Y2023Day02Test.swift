2876ccb74682f564c544a2a7738a616183c96167
Umair Saeed <umair.saeed@gmail.com>
2023-12-03 14:56:41 -0800

    [AoC][Swift][2023] Day 02: Part 1 & Part 2.

--- Swift/AoC-Swift/AoC-SwiftTests/2023/Y2023Day02Test.swift
+++ Swift/AoC-Swift/AoC-SwiftTests/2023/Y2023Day02Test.swift
@@ -7,25 +7,25 @@ final class Y2023Day02Test: XCTestCase {
   func testPart1_Example() throws {
     let day = Y2023Day02(dataFileNumber: 1)
     let lines = TestUtils().loadTestData(from: day.dataFilename())
-    XCTAssertEqual(day.part1(lines), "")
+    XCTAssertEqual(day.part1(lines), "8")
   }
 
   func testPart1_Full() throws {
     let day = Y2023Day02(dataFileNumber: 2)
     let lines = TestUtils().loadTestData(from: day.dataFilename())
-    XCTAssertEqual(day.part1(lines), "")
+    XCTAssertEqual(day.part1(lines), "2239")
   }
 
   func testPart2_Example() throws {
     let day = Y2023Day02(dataFileNumber: 1)
     let lines = TestUtils().loadTestData(from: day.dataFilename())
-    XCTAssertEqual(day.part2(lines), "")
+    XCTAssertEqual(day.part2(lines), "2286")
   }
 
   func testPart2_Full() throws {
     let day = Y2023Day02(dataFileNumber: 2)
     let lines = TestUtils().loadTestData(from: day.dataFilename())
-    XCTAssertEqual(day.part2(lines), "")
+    XCTAssertEqual(day.part2(lines), "83435")
   }
 }
 
