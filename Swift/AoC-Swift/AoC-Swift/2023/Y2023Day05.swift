0044b3a382bfc5fcc7008e46b4de93b61a8fd2b6
Umair Saeed <umair.saeed@gmail.com>
2023-12-05 05:43:16 -0800

    [AoC][Swift][2023] Day 05: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day05.swift
@@ -0,0 +1,23 @@
+// Created on 12/5/23.
+
+import Foundation
+
+/// --- Day 5: If You Give A Seed A Fertilizer ---
+/// https://adventofcode.com/2023/day/5
+class Y2023Day05: Day {
+  var dayNumber: Int = 05
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
