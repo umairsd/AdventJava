e1feda2b464aaa02bffc69370a08d30cf212f70d
Umair Saeed <umair.saeed@gmail.com>
2023-12-07 06:34:41 -0800

    [AoC][Swift][2023] Day 07: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day07.swift
@@ -0,0 +1,24 @@
+// Created on 12/7/23.
+
+import Foundation
+
+
+/// --- Day 7: Camel Cards ---
+/// https://adventofcode.com/2023/day/7
+class Y2023Day07: Day {
+  var dayNumber: Int = 07
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
