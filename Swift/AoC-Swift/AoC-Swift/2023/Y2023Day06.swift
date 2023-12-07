2b77fb1304dc9e602e203c1adb90005f684444f3
Umair Saeed <umair.saeed@gmail.com>
2023-12-06 19:16:24 -0800

    [AoC][Swift][2023] Day 06: Part 1.

--- Swift/AoC-Swift/AoC-Swift/2023/Y2023Day06.swift
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day06.swift
@@ -1,6 +1,7 @@
 // Created on 12/6/23.
 
 import Foundation
+import RegexBuilder
 
 /// --- Day 6: Wait For It ---
 /// https://adventofcode.com/2023/day/6
@@ -14,10 +15,67 @@ class Y2023Day06: Day {
   }
 
   func part1(_ lines: [String]) -> String {
-    ""
+    assert(lines.count >= 2)
+    let times = parseTimes(lines[0])
+    let distances = parseDistances(lines[1])
+
+    var waysToWin: [Int] = []
+    for (time, maxDistance) in zip(times, distances) {
+      let count = waysToCoverDistance(maxDistance, within: time)
+      waysToWin.append(count)
+    }
+
+    let result = waysToWin.reduce(1, *)
+    return "\(result)"
   }
+
 
   func part2(_ lines: [String]) -> String {
     ""
+  }
+
+
+  // MARK: - Private
+
+
+  private func waysToCoverDistance(_ maxDistance: Int, within time: Int) -> Int {
+    var distance = 0
+    var winCount = 0
+    for timeToHold in 0...time {
+      let timeRemaining = time - timeToHold
+      distance = timeToHold * timeRemaining
+      if distance > maxDistance {
+        winCount += 1
+      }
+    }
+
+    return winCount
+  }
+
+}
+
+
+// MARK: - Parsing
+
+extension Y2023Day06 {
+
+  func parseTimes(_ line: String) -> [Int] {
+    assert(line.starts(with: "Time: "))
+    return parseIntegers(line)
+  }
+
+  func parseDistances(_ line: String) -> [Int] {
+    assert(line.starts(with: "Distance:"))
+    return parseIntegers(line)
+  }
+
+
+  private func parseIntegers(_ line: String) -> [Int] {
+    let timeValues = line.trimmingCharacters(in: .whitespaces)
+      .split(separator: " ")
+      .dropFirst()
+      .map { $0.trimmingCharacters(in: .whitespaces) }
+      .compactMap { Int($0) }
+    return timeValues
   }
 }
