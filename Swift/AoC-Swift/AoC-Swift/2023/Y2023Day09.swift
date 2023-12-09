15d2a57af7f23413bb8cd92eda063a3f7cdf0a49
Umair Saeed <umair.saeed@gmail.com>
2023-12-09 11:26:10 -0800

    [AoC][Swift][2023] Day 09: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day09.swift
@@ -0,0 +1,24 @@
+// Created on 12/9/23.
+
+import Foundation
+
+/// --- Day 9: Mirage Maintenance ---
+///
+/// https://adventofcode.com/2023/day/9
+class Y2023Day09: Day {
+  var dayNumber: Int = 09
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
