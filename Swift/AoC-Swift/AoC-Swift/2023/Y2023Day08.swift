d35e0469aebdb86b86f5e5a0d8b9f07591716bfb
Umair Saeed <umair.saeed@gmail.com>
2023-12-09 06:41:58 -0800

    [AoC][Swift][2023] Day 08: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day08.swift
@@ -0,0 +1,24 @@
+// Created on 12/9/23.
+
+import Foundation
+
+
+/// --- Day 8: Haunted Wasteland ---
+/// https://adventofcode.com/2023/day/8
+class Y2023Day08: Day {
+  var dayNumber: Int = 08
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
