3daeb47af6891870ea58d7b5c22e5e5d6bf5ea6c
Umair Saeed <umair.saeed@gmail.com>
2023-12-10 06:25:08 -0800

    [AoC][Swift][2023] Day 10: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day10.swift
@@ -0,0 +1,26 @@
+// Created on 12/10/23.
+
+import Foundation
+
+
+/// --- Day 10: Pipe Maze ---
+///
+/// https://adventofcode.com/2023/day/10
+///
+class Y2023Day10: Day {
+  var dayNumber: Int = 10
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
