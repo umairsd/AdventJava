f7d94546e6e7c3f9dc6b13d7c7eb1cdbcc36533a
Umair Saeed <umair.saeed@gmail.com>
2023-12-20 06:42:59 -0800

    [AoC][Swift][2023] Day 11: Part 1 (Parsing + types)

--- Swift/AoC-Swift/AoC-Swift/2023/Y2023Day12.swift
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day12.swift
@@ -24,3 +24,58 @@ class Y2023Day12: Day {
     ""
   }
 }
+
+
+// MARK: - Helper Types
+
+fileprivate class SpringRow {
+  var springs: [SpringState]
+  var counts: [Int]
+
+  init(springs: [SpringState] = [], counts: [Int] = []) {
+    self.springs = springs
+    self.counts = counts
+  }
+}
+
+
+fileprivate enum SpringState: Character {
+  case unknown = "?"
+  case damaged = "#"
+  case operational = "."
+}
+
+
+// MARK: - Parsing
+
+extension Y2023Day12 {
+
+  fileprivate func parseSpringRow(_ line: String) -> SpringRow? {
+    let line = line.trimmingCharacters(in: .whitespacesAndNewlines)
+    guard !line.isEmpty else {
+      return nil
+    }
+
+    let segments = line.split(separator: " ")
+    guard segments.count == 2 else {
+      return nil
+    }
+    let springStates = parseSpringStates(String(segments[0]))
+    let counts = parseCounts(String(segments[1]))
+
+    let row = SpringRow(springs: springStates, counts: counts)
+    return row
+  }
+
+
+  fileprivate func parseSpringStates(_ line: String) -> [SpringState] {
+    let tokens = Array(line)
+    return tokens.compactMap { SpringState(rawValue: $0) }
+  }
+
+  fileprivate func parseCounts(_ line: String) -> [Int] {
+    let tokens = line.split(separator: ",")
+    return tokens.compactMap { Int($0.trimmingCharacters(in: .whitespaces)) }
+  }
+
+}
