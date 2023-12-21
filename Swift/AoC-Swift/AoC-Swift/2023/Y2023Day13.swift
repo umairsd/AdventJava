970c2bcd33a3745abbf7930e2accbb0b47d3fafd
Umair Saeed <umair.saeed@gmail.com>
2023-12-21 07:03:36 -0800

    [AoC][Swift][2023] Day 13: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day13.swift
@@ -0,0 +1,27 @@
+// Created on 12/21/23.
+
+import Foundation
+
+
+/// --- Day 13: Point of Incidence ---
+/// https://adventofcode.com/2023/day/13
+///
+class Y2023Day13: Day {
+  var dayNumber: Int = 13
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
