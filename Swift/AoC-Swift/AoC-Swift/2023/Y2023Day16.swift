cc5aaee91b98bcfa0e4ab745da334e2c61ca6e14
Umair Saeed <umair.saeed@gmail.com>
2023-12-27 06:48:53 -0800

    [AoC][Swift][2023] Day 16: Setup & Stubs.

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day16.swift
@@ -0,0 +1,26 @@
+// Created on 12/27/23.
+
+import Foundation
+
+/// --- Day 16: The Floor Will Be Lava ---
+/// https://adventofcode.com/2023/day/16
+/// 
+class Y2023Day16: Day {
+  var dayNumber: Int = 16
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
