e0a2ff0d56b59159fc84b6fdde603baf43c36801
Umair Saeed <umair.saeed@gmail.com>
2023-12-26 16:39:28 -0800

    [AoC][Swift][2023] Day 15: Part 1.

--- Swift/AoC-Swift/AoC-Swift/2023/Y2023Day15.swift
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day15.swift
@@ -16,11 +16,38 @@ class Y2023Day15: Day {
 
 
   func part1(_ lines: [String]) -> String {
-    ""
+    let tokens = parseTokens(lines[0])
+    let result = tokens.map { $0.hashValue() }.reduce(0, +)
+    return "\(result)"
   }
 
   
   func part2(_ lines: [String]) -> String {
     ""
+  }
+
+
+  private func parseTokens(_ line: String) -> [Token] {
+    let t = line.split(separator: ",")
+      .map { Token(data: String($0.trimmingCharacters(in: .whitespaces))) }
+    return t
+  }
+}
+
+
+fileprivate struct Token {
+  let data: String
+
+  func hashValue() -> Int {
+    var result = 0
+    for c in data {
+      guard let ascii = c.asciiValue else {
+        continue
+      }
+      result += Int(ascii)
+      result *= 17
+      result %= 256
+    }
+    return result
   }
 }
