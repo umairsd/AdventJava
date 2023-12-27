80e8540dfdacc96aa42447c37fb500bc1399ffb5
Umair Saeed <umair.saeed@gmail.com>
2023-12-26 16:30:48 -0800

    [AoC][Swift][2023] Day 15: Setup & Stubs

---
+++ Swift/AoC-Swift/AoC-Swift/2023/Y2023Day15.swift
@@ -0,0 +1,26 @@
+// Created on 12/26/23.
+
+import Foundation
+
+/// --- Day 15: Lens Library ---
+/// https://adventofcode.com/2023/day/15
+///
+class Y2023Day15: Day {
+  var dayNumber: Int = 15
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
