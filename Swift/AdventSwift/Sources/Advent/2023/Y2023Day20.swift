45004f45527d2c852ef0025ea0e60faab3e6ac3f
Umair Saeed <umair.saeed@gmail.com>
2024-01-02 06:33:52 -0800

    [AoC][Swift][2023] Day 20: Stubs

---
+++ Swift/AdventSwift/Sources/Advent/2023/Y2023Day20.swift
@@ -0,0 +1,27 @@
+// Created on 1/2/24.
+
+import Foundation
+
+
+/// --- Day 20: Pulse Propagation ---
+/// https://adventofcode.com/2023/day/20
+/// 
+class Y2023Day20: Day {
+  var dayNumber: Int = 20
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+
+  func part1(_ lines: [String]) -> String {
+    return ""
+  }
+
+
+  func part2(_ lines: [String]) -> String {
+    return ""
+  }
+}
