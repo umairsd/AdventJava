999f84a47e7ff24160dcb9f4c0031dd7a0d48ac6
Umair Saeed <umair.saeed@gmail.com>
2024-01-10 21:30:52 -0800

    [AoC][Swift][2023] Day 22: Stubs & Setup.

---
+++ Swift/AdventSwift/Sources/Advent/2023/Y2023Day22.swift
@@ -0,0 +1,23 @@
+// Created on 1/10/24.
+
+import Foundation
+
+/// --- Day 22: Sand Slabs ---
+/// https://adventofcode.com/2023/day/22
+class Y2023Day22: Day {
+  var dayNumber: Int = 22
+  var year: Int = 2023
+  var dataFileNumber: Int
+
+  required init(dataFileNumber: Int) {
+    self.dataFileNumber = dataFileNumber
+  }
+
+  func part1(_ lines: [String]) -> String {
+    ""
+  }
+
+  func part2(_ lines: [String]) -> String {
+    ""
+  }
+}
---
+++ Swift/AdventSwift/Tests/AdventTests/2023/Y2023Day22Test.swift
@@ -0,0 +1,32 @@
+// Created on 1/10/2024.
+
+import XCTest
+@testable import Advent
+
+final class Y2023Day22Test: XCTestCase {
+  private typealias DayType = Y2023Day22
+
+  func testPart1_Example() throws {
+    let day = DayType(dataFileNumber: 1)
+    let lines = day.readData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "")
+  }
+
+  func testPart1_Full() throws {
+    let day = DayType(dataFileNumber: 2)
+    let lines = day.readData(from: day.dataFilename())
+    XCTAssertEqual(day.part1(lines), "")
+  }
+
+  func testPart2_Example() throws {
+    let day = DayType(dataFileNumber: 1)
+    let lines = day.readData(from: day.dataFilename())
+    XCTAssertEqual(day.part2(lines), "")
+  }
+
+  func testPart2_Full() throws {
+    let day = DayType(dataFileNumber: 2)
+    let lines = day.readData(from: day.dataFilename())
+    XCTAssertEqual(day.part2(lines), "")
+  }
+}
