8a5deb74f4ca547dd037c1d3d6a42620dc0af4e0
Umair Saeed <umair.saeed@gmail.com>
2024-01-18 20:34:48 -0800

    [AoC][Swift][2023] Day 25: Still working on the Stoer-Wagner implementation.

--- Swift/AdventSwift/Sources/Advent/2023/Y2023Day25.swift
+++ Swift/AdventSwift/Sources/Advent/2023/Y2023Day25.swift
@@ -17,7 +17,8 @@ class Y2023Day25: Day {
 
 
   func part1(_ lines: [String]) -> String {
-    let graph = parseGraph(lines)
+//    let graph = parseGraph(lines)
+    let graph = buildSampleGraph()
     let minCut = Graph.minimumCut(graph)
     return ""
   }
@@ -26,245 +27,35 @@ class Y2023Day25: Day {
   func part2(_ lines: [String]) -> String {
     ""
   }
-}
 
+  private func buildSampleGraph() -> Graph {
+    let edges: [(Edge, Int)] = [
+      (Edge(source: "1", destination: "2"), 2),
+      (Edge(source: "1", destination: "5"), 3),
 
-// MARK: - Types
-
-fileprivate struct Edge: Hashable {
-  let source: String
-  let destination: String
-  let weight: Int
-
-  func hash(into hasher: inout Hasher) {
-    hasher.combine(source)
-    hasher.combine(destination)
-  }
-}
-
-
-fileprivate class Graph {
-  //  private let adjacencyList: [String: [String]]
-  private var adjacencyEdges: [String: [Edge]]
-  private var allVertices: Set<String>
-
-  init(adjacencyEdges: [String : [Edge]] = [:]) {
-    self.adjacencyEdges = adjacencyEdges
-    self.allVertices = Set(adjacencyEdges.keys)
-      .union(Set(adjacencyEdges.values.joined().map { $0.source }))
-      .union(adjacencyEdges.values.joined().map { $0.destination})
-  }
-
-  var vertexCount: Int {
-    return allVertices.count
-  }
-
-  func edge(from: String, to: String) -> Edge? {
-    guard let v = adjacencyEdges[from] else {
-      return nil
+      (Edge(source: "2", destination: "5"), 2),
+      (Edge(source: "2", destination: "6"), 2),
+      (Edge(source: "2", destination: "3"), 3),
+      
+      (Edge(source: "3", destination: "4"), 4),
+      (Edge(source: "3", destination: "7"), 2),
+      
+      (Edge(source: "4", destination: "8"), 2),
+      (Edge(source: "4", destination: "7"), 2),
+      
+      (Edge(source: "5", destination: "6"), 3),
+      
+      (Edge(source: "6", destination: "7"), 1),
+      
+      (Edge(source: "7", destination: "8"), 3),
+    ]
+    var graph = Graph()
+    for (e, w) in edges {
+      graph.addEdge(from: e.source, to: e.destination, weight: w)
+      graph.addEdge(from: e.destination, to: e.source, weight: w)
     }
-    return v.first { $0.destination == to }
+    return graph
   }
-
-  func vertices() -> [String] {
-    return Array(allVertices)
-  }
-
-  func edges() -> [Edge] {
-    Array(adjacencyEdges.values.joined())
-  }
-
-  func containsVertex(_ v: String) -> Bool {
-    allVertices.contains(v)
-  }
-
-  func addEdge(from source: String, to destination: String, weight: Int) {
-    var x = adjacencyEdges[source, default: []]
-    x.append(Edge(source: source, destination: destination, weight: weight))
-    adjacencyEdges[source] = x
-
-    var y = adjacencyEdges[destination, default: []]
-    y.append(Edge(source: destination, destination: source, weight: weight))
-    adjacencyEdges[destination] = y
-  }
-
-
-  func graphByMergingVertex(_ t: String, into s: String) -> Graph {
-    let originalGraph = self
-    let mergedNodeName = "\(s),\(t)"
-
-    var adjacencyEdges: [String: [Edge]] = originalGraph.adjacencyEdges
-
-    // Move all edges from `x` to `t` `Edge(x-t)` so that they point to `s`, i.e. `Edge(x-s)`.
-    // If there already is an `Edge(x-s)`, then update its weight by adding the weight
-    // of `Edge(x-t)`.
-    let edgesFromT = adjacencyEdges.removeValue(forKey: t)!
-
-    for edgeFromT in edgesFromT {
-      let x = edgeFromT.destination
-
-      var edgesFromX = adjacencyEdges[x, default: []]
-      guard let xToT = edgesFromX.filter({ $0.destination == t }).first else {
-        continue
-      }
-
-      // Remove all edges from (x-t).
-      edgesFromX.removeAll { $0.destination == t }
-
-      if let xToS = edgesFromX.filter({ $0.destination == s }).first {
-        edgesFromX.removeAll { $0.destination == s }
-        let newXToS = Edge(source: x, destination: s, weight: xToS.weight + xToT.weight)
-        edgesFromX.append(newXToS)
-      } else {
-        let newXToS = Edge(source: x, destination: s, weight: xToT.weight)
-        edgesFromX.append(newXToS)
-      }
-
-      // TODO: Rename the vertex to `mergedNodeName`
-      adjacencyEdges[x] = edgesFromX
-    }
-
-    let newGraph = Graph(adjacencyEdges: adjacencyEdges)
-    return newGraph
-  }
-}
-
-
-
-fileprivate extension Graph {
-
-  struct CutPhaseResult {
-    let vertexS: String
-    let vertexT: String
-    let weight: Int
-  }
-
-  struct MinCutResult {
-    let first: Graph
-    let second: Graph
-    let edgesOnTheCut: [Edge]
-    let cutWeight: Int
-  }
-
-
-  /// MinimumCutPhase, aka "maximum adjacency search", which results in finding two
-  /// vertices `s` and `t`, which are the last two vertices that were found during the 
-  /// search. In addition, we return the sum of the weights of the edges connecting
-  /// to `t` from the rest of the graph, including `s`.
-  ///
-  /// ```
-  /// MinimumCutPhase(G, a):
-  ///   A <- {a}
-  ///   while A != V:
-  ///     add to A the most tightly connected vertex
-  ///   return s, t, and the cut weight as the "cut of the phase"
-  /// ```
-  ///
-  static func minimumCutPhase(_ graph: Graph) -> CutPhaseResult {
-    let vertices: [String] = graph.vertices()
-
-    let start = vertices[0]
-    var foundSet = [start]
-    var cutWeight: [Int] = []
-    var candidates: Set<String> = Set(vertices)
-    candidates.remove(start)
-
-    while !candidates.isEmpty {
-      var maxNextVertex: String!
-      var maxWeight = Int.min
-
-      for nextVertex in candidates {
-        var weightSum = 0
-        for s in foundSet {
-          guard let edge = graph.edge(from: nextVertex, to: s) else {
-            continue
-          }
-          weightSum += edge.weight
-        }
-
-        if weightSum > maxWeight {
-          maxNextVertex = nextVertex
-          maxWeight = weightSum
-        }
-      }
-
-      candidates.remove(maxNextVertex)
-      foundSet.append(maxNextVertex)
-      cutWeight.append(maxWeight)
-    }
-
-    assert(foundSet.count >= 2)
-    let n = foundSet.count
-    // Take the last two vertices and their weight as a cut of the phase.
-    return CutPhaseResult(
-      vertexS: foundSet[n - 2],
-      vertexT: foundSet[n - 1],
-      weight: cutWeight.last!)
-  }
-
-
-
-  static func minimumCut(_ g: Graph) -> MinCutResult {
-    var graph = g
-    var currentPartition = Set<String>()
-    var bestPartition = Set<String>()
-    var bestCut: CutPhaseResult!
-
-    while graph.vertexCount > 1 {
-      let currentCut = minimumCutPhase(graph)
-      if bestCut == nil || currentCut.weight < bestCut.weight {
-        bestCut = currentCut
-        bestPartition = currentPartition
-        bestPartition.insert(bestCut.vertexT)
-      }
-      currentPartition.insert(currentCut.vertexT)
-      graph = graph.graphByMergingVertex(currentCut.vertexT, into: currentCut.vertexS)
-    }
-
-    // `bestPartition` now contains our first partition.
-    let r = constructMinCutResult(g, partition: bestPartition, bestCut: bestCut)
-    return r
-  }
-
-
-  static func constructMinCutResult(
-    _ originalGraph: Graph,
-    partition: Set<String>,
-    bestCut: CutPhaseResult
-  ) -> MinCutResult {
-
-    let first = Graph()
-    let second = Graph()
-
-    var cuttingEdges: [Edge] = []
-    var cutWeight = 0
-
-    for v in originalGraph.vertices() {
-      if partition.contains(v) {
-        // first.addVertex(v)
-      } else {
-        // second.addVertex(v)
-      }
-    }
-
-    for e in originalGraph.edges() {
-      if first.containsVertex(e.source) && first.containsVertex(e.destination) {
-        // first.addEdge(e.source, e.destination, e.weight)
-      } else if second.containsVertex(e.source) && second.containsVertex(e.destination) {
-        // second.addEdge(e.source, e.destination, e.weight)
-      } else {
-        cuttingEdges.append(Edge(source: e.source, destination: e.destination, weight: e.weight))
-        cutWeight += e.weight
-      }
-    }
-
-    return MinCutResult(
-      first: first,
-      second: second,
-      edgesOnTheCut: cuttingEdges,
-      cutWeight: cutWeight)
-  }
-
 }
 
 
@@ -309,11 +100,13 @@ fileprivate extension Y2023Day25 {
 
 
   func parseGraph(_ lines: [String]) -> Graph {
-    var graph: [String: [Edge]] = [:]
+    var graph = Graph()
+
     for line in lines {
       guard !line.isEmpty else {
         continue
       }
+
       let nodes = parseLine(line)
       // First node is the src.
       assert(nodes.count > 0)
@@ -321,19 +114,333 @@ fileprivate extension Y2023Day25 {
       let destinationNames = nodes.suffix(from: 1)
 
       for destinationName in destinationNames {
-        let e1 = Edge(source: srcName, destination: destinationName, weight: 1)
-        let e2 = Edge(source: destinationName, destination: srcName, weight: 1)
-        
-        var srcNeighbors = graph[srcName, default: []]
-        srcNeighbors.append(e1)
-        graph.updateValue(srcNeighbors, forKey: srcName)
-
-        var destinationNeighbors = graph[destinationName, default: []]
-        destinationNeighbors.append(e2)
-        graph.updateValue(destinationNeighbors, forKey: destinationName)
+        graph.addEdge(from: srcName, to: destinationName, weight: 1)
+        graph.addEdge(from: destinationName, to: srcName, weight: 1)
       }
     }
 
-    return Graph(adjacencyEdges: graph)
+    return graph
   }
+}
+
+
+
+// MARK: - Types
+
+fileprivate struct Edge: Hashable {
+  let source: String
+  let destination: String
+
+  func reversed() -> Edge {
+    return Edge(source: destination, destination: source)
+  }
+}
+
+
+fileprivate struct Graph {
+
+  private var adjacencyList: [String: [Edge]] = [:]
+  private var allVertices = Set<String>()
+  private var weights: [Edge: Int] = [:]
+
+  // MARK: Properties
+
+  // Returns all the vertices in the graph.
+  var vertices: Set<String> {
+    allVertices
+  }
+
+  var vertexCount: Int {
+    allVertices.count
+  }
+
+  func weightFor(_ e: Edge) -> Int {
+    return weights[e]!
+  }
+
+  /// Add or update an edge.
+  mutating func addEdge(from source: String, to destination: String, weight: Int) {
+    allVertices.insert(source)
+    allVertices.insert(destination)
+
+    let e = Edge(source: source, destination: destination)
+    var edges = adjacencyList[source, default: []]
+
+    if let index = edges.firstIndex(of: e) {
+      // Remove the old edge.
+      edges.remove(at: index)
+    }
+
+    edges.append(e)
+    adjacencyList[source] = edges
+    weights[e] = weight
+  }
+
+
+  func containsVertex(_ v: String) -> Bool {
+    allVertices.contains(v)
+  }
+
+  func containsEdge(_ e: Edge) -> Bool {
+    let edges = adjacencyList[e.source, default: []]
+    return edges.contains { $0.destination == e.destination }
+  }
+
+
+  func graphByMergingVertex(_ t: String, into s: String) -> Graph {
+    let originalGraph = self
+    var newGraph = Graph()
+    var adjacencyList = originalGraph.adjacencyList
+
+    // 1. Remove the edge from s-t, and t-s.
+    for (source, edges) in adjacencyList {
+      guard source == t || source == s else {
+        continue
+      }
+      var mutatedEdges: [Edge] = []
+      for edge in edges {
+        if (edge.source == t && edge.destination == s) ||
+            (edge.destination == t && edge.source == s) {
+          continue
+        }
+        mutatedEdges.append(edge)
+      }
+      adjacencyList[source] = mutatedEdges
+    }
+
+    assert(adjacencyList.values.joined().count % 2 == 0)
+
+
+    // 2. Remove all edges (t-x) and gather in `edgesFromT`.
+    let edgesFromT = adjacencyList.removeValue(forKey: t) ?? []
+    let neighborsOfT = Set<String>(edgesFromT.map({ $0.destination }))
+
+    // 3. Remove all the edges (x-t).
+    for v in neighborsOfT {
+      let edges = adjacencyList[v, default: []]
+      let mutatedEdges = Array(edges.filter { $0.destination != t })
+      adjacencyList[v] = mutatedEdges
+    }
+
+    assert(adjacencyList.values.joined().count % 2 == 0)
+
+    // 4. At this point, no edges (x-t) or (t-x) exist. Go through all the remaining
+    // edges, and add them to the new graph.
+    let allEdges = adjacencyList.values.joined()
+    for e in allEdges {
+      let w = originalGraph.weightFor(e)
+      newGraph.addEdge(from: e.source, to: e.destination, weight: w)
+    }
+
+    // 5. Finally, add all the edges in `edgesFromT` (and their inverses) such that
+    // they all point to `s`.
+    for edgeFromT in edgesFromT {
+      assert(originalGraph.containsEdge(edgeFromT))
+      let weightTtoX = originalGraph.weightFor(edgeFromT)
+
+      let edgeXtoS = Edge(source: edgeFromT.destination, destination: s)
+      let edgeStoX = Edge(source: s, destination: edgeFromT.destination)
+
+      var weight = weightTtoX
+      if originalGraph.containsEdge(edgeXtoS) {
+        assert(originalGraph.containsEdge(edgeStoX))
+        let w = originalGraph.weightFor(edgeXtoS)
+        weight += w
+      }
+
+      newGraph.addEdge(from: s, to: edgeFromT.destination, weight: weight)
+      newGraph.addEdge(from: edgeFromT.destination, to: s, weight: weight)
+    }
+
+    return newGraph
+  }
+}
+
+
+// MARK: - Stoer-Wagner Minimum Cut Algorithm
+
+fileprivate extension Graph {
+
+  struct CutPhaseResult {
+    let vertexS: String
+    let vertexT: String
+    let weight: Int
+  }
+
+  struct MinCutResult {
+    let first: Graph
+    let second: Graph
+    let edgesOnTheCut: [Edge]
+    let cutWeight: Int
+  }
+
+
+  /// MinimumCutPhase, aka "maximum adjacency search", which results in finding two
+  /// vertices `s` and `t`, which are the last two vertices that were found during the
+  /// search. In addition, we return the sum of the weights of the edges connecting
+  /// to `t` from the rest of the graph, including `s`.
+  ///
+  /// ```
+  /// MinimumCutPhase(G, W, a):
+  ///   A <- {a}
+  ///   while A != V:
+  ///     add to A the most tightly connected vertex
+  ///
+  ///   store the cut in which the last remaining vertex is by itself (i.e. CutOfThePhase)
+  ///   shrink G by merging the two vertices (s, t) added last
+  ///       (the value of "cut-of-the-phase" is the value of minimum s, t cut.)
+  ///
+  ///   return s, t, and the cut weight as the "cut of the phase"
+  /// ```
+  ///
+  /// ```
+  /// MinimumCut(G, W, a):
+  ///   while |V| > 1
+  ///      MinimumCutPhase(G, W, a)
+  ///      if the cut-of-the-phase is lighter than the current minimum cut
+  ///         then store the cut-of-the-phase as the current minimum cut
+  /// ```
+  ///
+  static func minimumCutPhase(_ graph: Graph) -> CutPhaseResult {
+    guard let start = graph.vertices.first else {
+      fatalError()
+    }
+
+    var foundSet = [start] // Set A
+    var cutWeight: [Int] = []
+    var candidates: Set<String> = Set(graph.vertices)
+    candidates.remove(start)
+
+    // while A != V
+    //     Add to `foundSet` the most tightly connected vertex.
+    while !candidates.isEmpty {
+      var mostTightlyConnectedVertex: String!
+      var maxWeight = Int.min
+
+      // Find the most tightly connected vertex.
+      // *Tightly connected vertex* to `foundSet` implies a vertex whose edge weight
+      // to any of the vertex in `foundSet` is maximum.
+      for vertex in candidates {
+        // The total weight of the vertex to all vertices in the `foundSet`.
+        var totalVertexWeight = 0
+        for s in foundSet {
+          let e = Edge(source: vertex, destination: s)
+          guard graph.containsEdge(e) else {
+            continue
+          }
+          totalVertexWeight += graph.weightFor(e)
+        }
+
+        if totalVertexWeight > maxWeight {
+          mostTightlyConnectedVertex = vertex
+          maxWeight = totalVertexWeight
+        }
+      }
+
+      candidates.remove(mostTightlyConnectedVertex)
+      foundSet.append(mostTightlyConnectedVertex)
+      cutWeight.append(maxWeight)
+    }
+
+    assert(foundSet.count >= 2)
+    let n = foundSet.count
+    // Take the last two vertices and their weight as a cut of the phase.
+    let result = CutPhaseResult(
+      vertexS: foundSet[n - 2],
+      vertexT: foundSet[n - 1],
+      weight: cutWeight.last!)
+    return result
+  }
+
+
+  static func minimumCut(_ originalGraph: Graph) -> MinCutResult {
+    var graph = originalGraph
+
+    var bestPhase = 0
+    var bestCutWeight = Int.max
+    var contractions: [(String, String)] = []
+
+    while graph.vertexCount > 1 {
+      let currentCut = minimumCutPhase(graph)
+
+      if currentCut.weight < bestCutWeight {
+        bestCutWeight = currentCut.weight
+      }
+
+      contractions.append((currentCut.vertexS, currentCut.vertexT))
+      graph = graph.graphByMergingVertex(currentCut.vertexT, into: currentCut.vertexS)
+    }
+
+
+//    var currentPartition = Set<String>()
+//    var bestPartition: Set<String>!
+//    var bestCut: CutPhaseResult!
+//
+//    while graph.vertexCount > 1 {
+//      let cutResult = minimumCutPhase(graph)
+//
+//      if bestCut == nil || bestCut.weight < cutResult.weight {
+//        bestCut = cutResult
+//        bestPartition = Set(currentPartition)
+//        bestPartition.insert(cutResult.vertexT)
+//      }
+//
+//      currentPartition.insert(cutResult.vertexT)
+//      graph = graph.graphByMergingVertex(cutResult.vertexT, into: cutResult.vertexS)
+//    }
+
+    fatalError()
+  }
+
+
+  // we do a two-pass algorithm to reconstruct the sub-graphs:
+  // - first we distribute the vertices into their respective graph
+  // - second we align edges: if they cross graphs they will end in the list.
+  static func constructMinCutResult(
+    _ originalGraph: Graph,
+    partition: Set<String>
+  ) -> MinCutResult {
+
+    var first = Graph()
+    var firstVertices = Set<String>()
+    var second = Graph()
+    var secondVertices = Set<String>()
+
+    var cuttingEdges: [Edge] = []
+    var cutWeight = 0
+
+    for v in originalGraph.allVertices {
+      if partition.contains(v) {
+        firstVertices.insert(v)
+      } else {
+        secondVertices.insert(v)
+      }
+    }
+
+    for (_, edges) in originalGraph.adjacencyList {
+      for edge in edges {
+        let w = originalGraph.weightFor(edge)
+
+        if firstVertices.contains(edge.source) && firstVertices.contains(edge.destination) {
+          first.addEdge(from: edge.source, to: edge.destination, weight: w)
+          first.addEdge(from: edge.destination, to: edge.source, weight: w)
+
+        } else if secondVertices.contains(edge.source) && secondVertices.contains(edge.destination) {
+          second.addEdge(from: edge.source, to: edge.destination, weight: w)
+          second.addEdge(from: edge.destination, to: edge.source, weight: w)
+
+        } else {
+          cuttingEdges.append(edge)
+          cutWeight += w
+        }
+      }
+    }
+
+    return MinCutResult(
+      first: first,
+      second: second,
+      edgesOnTheCut: cuttingEdges,
+      cutWeight: cutWeight)
+  }
+
 }
